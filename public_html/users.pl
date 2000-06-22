9f52b02235882a7e0fa737f5067a006c7bc44523
pudge <pudge>
2000-06-22 03:28:43 +0000

    Another nickname fix.  The Strong Arm of Slash is coming down upon you.

--- public_html/users.pl
+++ public_html/users.pl
@@ -21,7 +21,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 #
-#  $Id: users.pl,v 1.11 2000/06/19 20:41:00 pudge Exp $
+#  $Id: users.pl,v 1.12 2000/06/22 03:28:43 pudge Exp $
 ###############################################################################
 use strict;
 use lib '../';
@@ -226,8 +226,7 @@ sub newUser {
 	# Check if User Exists
 
 	$I{F}{newuser} =~ s/\s+/ /g;
-	$I{F}{newuser} = stripByMode($I{F}{newuser}, "nohtml");
-	$I{F}{newuser} =~ s/[?&%#=]//g;
+	$I{F}{newuser} =~ s/[^ 0-9a-zA-Z$_.+!*'(),-]+//g;
 	$I{F}{newuser} = substr($I{F}{newuser}, 0, 20);
 
 	(my $matchname = lc $I{F}{newuser}) =~ s/[^a-zA-Z0-9]//g;
@@ -1006,8 +1005,8 @@ EOT1
 EOT2
 
 	print <<EOT;
-	(Note: the characters &amp;&lt;&gt;?%#= are not allowed in nicknames, and
-	will be stripped out.)
+	(Note: only the characters <TT>0-9a-zA-Z$_.+!*'(),-</TT>, plus space,
+	are allowed in nicknames, and all others will be stripped out.)
 
 	<INPUT TYPE="TEXT" NAME="newuser" SIZE="20" MAXLENGTH="20" VALUE="$I{F}{newuser}">
 	<BR> and a <B>valid email address</B> address to send your registration

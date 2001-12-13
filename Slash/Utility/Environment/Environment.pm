6bc82027f416072d33cb2101ada670ea5b7d9b86
jamie <jamie>
2001-12-13 17:42:37 +0000

    Added parameter "type".

--- Slash/Utility/Environment/Environment.pm
+++ Slash/Utility/Environment/Environment.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2001 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: Environment.pm,v 1.7 2001/12/12 19:20:10 pudge Exp $
+# $Id: Environment.pm,v 1.8 2001/12/13 17:42:37 jamie Exp $
 
 package Slash::Utility::Environment;
 
@@ -31,7 +31,7 @@ use Digest::MD5 'md5_hex';
 use base 'Exporter';
 use vars qw($VERSION @EXPORT);
 
-($VERSION) = ' $Revision: 1.7 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.8 $ ' =~ /\$Revision:\s+([^\s]+)/;
 @EXPORT	   = qw(
 	createCurrentAnonymousCoward
 	createCurrentCookie
@@ -1199,7 +1199,7 @@ sub filter_params {
 
 	# fields that have ONLY a-zA-Z0-9_
 	my %alphas = map {($_ => 1)} qw(
-		mode section
+		mode section type
 	);
 
 	# regexes to match dynamically generated numeric fields
@@ -1546,4 +1546,4 @@ Slash(3), Slash::Utility(3).
 
 =head1 VERSION
 
-$Id: Environment.pm,v 1.7 2001/12/12 19:20:10 pudge Exp $
+$Id: Environment.pm,v 1.8 2001/12/13 17:42:37 jamie Exp $

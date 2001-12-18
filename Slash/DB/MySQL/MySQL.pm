25cd6db989fd39e3f306b6dd742880ca26bd67b4
jamie <jamie>
2001-12-18 18:28:11 +0000

    A questioning comment.

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2001 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.39 2001/12/14 17:25:41 jamie Exp $
+# $Id: MySQL.pm,v 1.40 2001/12/18 18:28:11 jamie Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -16,7 +16,7 @@ use vars qw($VERSION);
 use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 
-($VERSION) = ' $Revision: 1.39 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.40 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -2940,6 +2940,7 @@ sub countStoriesTopHits {
 sub countStorySubmitters {
 	my($self) = @_;
 
+	# Shouldn't this just be getCurrentStatic('anonymous_coward_uid') ? - Jamie
 	my $ac_uid = getCurrentAnonymousCoward('uid');
 	my $uid = $self->sqlSelectColArrayref('uid', 'authors_cache');
 	push @$uid, $ac_uid;

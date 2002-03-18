69e3a6e581659d53b01ac515ab8c493693609953
brian <brian>
2002-03-18 22:55:01 +0000

    Added addition to ts

--- plugins/Stats/Stats.pm
+++ plugins/Stats/Stats.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2002 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: Stats.pm,v 1.11 2002/03/18 22:26:47 brian Exp $
+# $Id: Stats.pm,v 1.12 2002/03/18 22:55:01 brian Exp $
 
 package Slash::Stats;
 
@@ -15,7 +15,7 @@ use vars qw($VERSION);
 use base 'Slash::DB::Utility';
 use base 'Slash::DB::MySQL';
 
-($VERSION) = ' $Revision: 1.11 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.12 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # On a side note, I am not sure if I liked the way I named the methods either.
 # -Brian
@@ -155,23 +155,23 @@ sub countCommentsDaily {
 sub countDailyComments {
 	my($self, $yesterday) = @_;
 	$self->sqlSelect("count(*)", "accesslog",
-		"op='comments' AND BETWEEN '$yesterday 00:00' AND '$yesterday 23:59:59' GROUP BY op");
+		"op='comments' AND ts BETWEEN '$yesterday 00:00' AND '$yesterday 23:59:59'");
 }
 
 ########################################################
 sub countDailyArticles {
 	my($self, $yesterday) = @_;
 	$self->sqlSelect("count(*)", "accesslog",
-		"op='comments' AND BETWEEN '$yesterday 00:00' AND '$yesterday 23:59:59'",
-	 'GROUP BY op');
+		"op='articles' AND ts BETWEEN '$yesterday 00:00' AND '$yesterday 23:59:59'",
+	 '');
 }
 
 ########################################################
 sub countDailyCommentsByDistinctIPID {
 	my($self, $yesterday) = @_;
 	$self->sqlSelect("count(*)", "accesslog",
-		"op='comments' AND BETWEEN '$yesterday 00:00' AND '$yesterday 23:59:59'", 
-		'GROUP BY op',
+		"op='comments' AND ts BETWEEN '$yesterday 00:00' AND '$yesterday 23:59:59'", 
+		'',
 		{distinct => 1});
 }
 
@@ -179,8 +179,8 @@ sub countDailyCommentsByDistinctIPID {
 sub countDailyArticlesByDistinctIPID {
 	my($self, $yesterday) = @_;
 	$self->sqlSelect("count(*)", "accesslog",
-		"op='comments' AND BETWEEN '$yesterday 00:00' AND '$yesterday 23:59:59'",
-		'GROUP BY op',
+		"op='articles' AND ts BETWEEN '$yesterday 00:00' AND '$yesterday 23:59:59'",
+		'',
 		{distinct => 1});
 }
 
@@ -205,7 +205,7 @@ sub countDaily {
 	$c->finish;
 
 	$returnable{'journals'}  = $self->sqlSelect("count(*)", "accesslog",
-		"op='journal' AND to_days(now()) - to_days(ts)=1 GROUP BY op");
+		"op='journal' AND to_days(now()) - to_days(ts)=1 ");
 
 	$c = $self->sqlSelectMany("dat,count(*)", "accesslog",
 		"to_days(now()) - to_days(ts)=1 AND

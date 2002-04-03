24f5dc09129edd6c2f0a25f87c374bd6f8aea8d8
jamie <jamie>
2002-04-03 19:39:31 +0000

    Ask banned people to put their IP in the subject line;  put the banned@slashdot.org address along with the FAQ entries into the "you are banned" message.

--- Slash/Apache/Banlist/Banlist.pm
+++ Slash/Apache/Banlist/Banlist.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2002 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: Banlist.pm,v 1.5 2002/04/03 19:37:35 jamie Exp $
+# $Id: Banlist.pm,v 1.6 2002/04/03 19:39:31 jamie Exp $
 
 package Slash::Apache::Banlist;
 
@@ -11,7 +11,7 @@ use Digest::MD5 'md5_hex';
 use Apache::Constants qw(:common);
 use vars qw($VERSION);
 
-($VERSION) = ' $Revision: 1.5 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.6 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 sub handler {
 	my($r) = @_;
@@ -77,7 +77,7 @@ Last Modified: 3/26/02<br>
 </small></i> <a name="ac1000"></a>  
 <h2>How do I get an IP Unbanned?</h2> 
 <p>Email <a href="mailto:banned@slashdot.org">banned@slashdot.org</a>. Make  
-sure to include the IP in question, and any other pertinant information. If  
+sure to include the IP in question, and any other pertinent information. If  
 you are connecting through a proxy server, you might need to have your proxy  
 server's admin contact us instead of you. <br> 
 <br> 

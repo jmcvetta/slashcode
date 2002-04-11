d7766ce3080158f0bc435e0271d7e496483a8152
jamie <jamie>
2002-04-11 16:14:30 +0000

    Fix to handle crashed slashd's better.

--- Slash/DB/Static/MySQL/MySQL.pm
+++ Slash/DB/Static/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2002 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.27 2002/04/11 04:56:59 jamie Exp $
+# $Id: MySQL.pm,v 1.28 2002/04/11 16:14:30 jamie Exp $
 
 package Slash::DB::Static::MySQL;
 #####################################################################
@@ -16,7 +16,7 @@ use URI ();
 use vars qw($VERSION);
 use base 'Slash::DB::MySQL';
 
-($VERSION) = ' $Revision: 1.27 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.28 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # FRY: Hey, thinking hurts 'em! Maybe I can think of a way to use that.
 
@@ -1046,10 +1046,14 @@ sub setSlashdStatus {
 ########################################################
 sub createSlashdStatus {
 	my($self, $taskname) = @_;
-	return $self->sqlInsert(
+	$self->sqlInsert(
 		"slashd_status",
 		{ task => $taskname },
 		{ ignore => 1 } );
+	$self->sqlUpdate(
+		"slashd_status",
+		{ in_progress => 0 },
+		"task=" . $self->sqlQuote($taskname));
 }
 
 ########################################################

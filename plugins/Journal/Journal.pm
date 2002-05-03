ab6cb9d8bae7918cfdaa4f698f1ef9f56d7fae3e
brian <brian>
2002-05-03 02:23:14 +0000

    New search stuff

--- plugins/Journal/Journal.pm
+++ plugins/Journal/Journal.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2002 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: Journal.pm,v 1.23 2002/02/08 20:25:54 brian Exp $
+# $Id: Journal.pm,v 1.24 2002/05/03 02:23:14 brian Exp $
 
 package Slash::Journal;
 
@@ -16,7 +16,7 @@ use base 'Exporter';
 use base 'Slash::DB::Utility';
 use base 'Slash::DB::MySQL';
 
-($VERSION) = ' $Revision: 1.23 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.24 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # On a side note, I am not sure if I liked the way I named the methods either.
 # -Brian
@@ -221,7 +221,7 @@ sub searchUsers {
 	);
 	return unless @$find;
 
-	$uids   = join(" OR ", map { "uid=$_->[2]" } @$find);
+	$uids   = join(" OR ", map { "uid=$_->{uid}" } @$find);
 	$jusers = $self->sqlSelectAllHashref(
 		'uid', 'uid, MAX(id) as id', 'journals', $uids, 'GROUP BY uid'
 	);
@@ -231,7 +231,7 @@ sub searchUsers {
 		'uid', 'uid, id, date, description', 'journals', $ids
 	);
 
-	for my $user (sort { lc $a->[1] cmp lc $b->[1] } @$find) {
+	for my $user (sort { lc $a->{nickname} cmp lc $b->{nickname} } @$find) {
 		my $uid  = $user->[2];
 		my $nick = $user->[1];
 		if (exists $journals->{$uid}) {

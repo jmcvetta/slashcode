9440185ee4735d4aaa9cd8dae48a2cab2b77116e
pater <pater>
2002-07-15 15:38:46 +0000

    'rss' was hardcoded into Stats.pm, this is now the var $op_exclude_from_countdaily

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2002 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.186 2002/07/15 05:14:46 jamie Exp $
+# $Id: MySQL.pm,v 1.187 2002/07/15 15:38:46 pater Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -15,7 +15,7 @@ use vars qw($VERSION);
 use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 
-($VERSION) = ' $Revision: 1.186 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.187 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -4559,6 +4559,7 @@ sub getSlashConf {
 		lonetags =>			[qw( P LI BR IMG )],
 		fixhrefs =>			[ ],
 		lonetags =>			[ ],
+		op_exclude_from_countdaily =>   [qw( rss )],
 		reasons =>			[qw( Normal Offtopic Flamebait Troll Redundant
 						     Insightful Interesting Informative Funny
 						     Overrated Underrated )],

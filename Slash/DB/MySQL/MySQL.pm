02e11f937dea95c203c947afc8f98a69dc8dc261
brian <brian>
2002-10-11 17:16:21 +0000

    Removing WHERE stuff

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2002 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.234 2002/10/11 17:12:58 jamie Exp $
+# $Id: MySQL.pm,v 1.235 2002/10/11 17:16:21 brian Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -15,7 +15,7 @@ use vars qw($VERSION);
 use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 
-($VERSION) = ' $Revision: 1.234 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.235 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -3093,19 +3093,19 @@ sub getAccessListInfo {
 
 	if ($user_check) {
 		if ($user_check->{uid} =~ /^\d+$/ && !isAnon($user_check->{uid})) {
-			$where = "WHERE uid = $user_check->{uid}";
+			$where = "uid = $user_check->{uid}";
 		} elsif ($user_check->{md5id}) {
-			$where = "WHERE ipid = '$user_check->{md5id}'";
+			$where = "ipid = '$user_check->{md5id}'";
 		} elsif ($user_check->{ipid}) {
-			$where = "WHERE ipid = '$user_check->{ipid}'";
+			$where = "ipid = '$user_check->{ipid}'";
 		} elsif ($user_check->{subnetid}) {
-			$where = "WHERE subnetid = '$user_check->{subnetid}'";
+			$where = "subnetid = '$user_check->{subnetid}'";
 		} else {
 			return {};
 		}
 	} else {
 		$user_check = $self->getCurrentUser();
-		$where = "WHERE (ipid = '$user_check->{ipid}' OR subnetid = '$user_check->{subnetid}')";
+		$where = "(ipid = '$user_check->{ipid}' OR subnetid = '$user_check->{subnetid}')";
 	}
 
 	if ($column eq 'isbanned') {
@@ -3115,7 +3115,7 @@ sub getAccessListInfo {
 	}
 	
 
-	$ref = $self->sqlSelectAll("reason, ts", "accesslist $where");
+	$ref = $self->sqlSelectAll("reason, ts", 'accesslist', $where);
 	$aclinfo->{reason} = '';
 
 	for (@$ref) {

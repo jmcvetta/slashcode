10b4e4000bef150dd09a5d37e65a46e09049c340
pudge <pudge>
2002-10-30 18:20:42 +0000

    Strip description

--- plugins/Journal/journal.pl
+++ plugins/Journal/journal.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2002 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: journal.pl,v 1.55 2002/10/25 05:32:46 jamie Exp $
+# $Id: journal.pl,v 1.56 2002/10/30 18:20:42 pudge Exp $
 
 use strict;
 use Slash 2.003;	# require Slash 2.3.x
@@ -12,7 +12,7 @@ use Slash::Utility;
 use Slash::XML;
 use vars qw($VERSION);
 
-($VERSION) = ' $Revision: 1.55 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.56 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 sub main {
 	my $journal   = getObject('Slash::Journal');
@@ -511,6 +511,7 @@ sub listArticle {
 
 sub saveArticle {
 	my($journal, $constants, $user, $form, $slashdb, $ws) = @_;
+	$form->{description} =~ s/[\r\n].*$//s;  # strip anything after newline
 	my $description = strip_notags($form->{description});
 
 	unless ($description ne "" && $form->{article} ne "") {

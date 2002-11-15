91a56090008b7bbbda994d095b294c0445db91fe
brian <brian>
2002-11-15 19:10:36 +0000

    Dumb mistake

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2002 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.249 2002/11/15 18:16:03 brian Exp $
+# $Id: MySQL.pm,v 1.250 2002/11/15 19:10:36 brian Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -15,7 +15,7 @@ use vars qw($VERSION);
 use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 
-($VERSION) = ' $Revision: 1.249 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.250 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -4654,10 +4654,10 @@ sub getSubmissionForUser {
 	# Mail me about questions. -Brian
 	my $SECT = $self->getSection($user->{section} ? $user->{section} : $form->{section});
 	if ($SECT->{type} eq 'collected') {
-		push @where, " AND section IN ('" . join("','", @{$SECT->{contained}}) . "')" 
+		push @where, " section IN ('" . join("','", @{$SECT->{contained}}) . "')" 
 			if $SECT->{contained} && @{$SECT->{contained}};
 	} else {
-		push @where, " AND section = " . $self->sqlQuote($SECT->{section});
+		push @where, " section = " . $self->sqlQuote($SECT->{section});
 	}
 	
 	my $submissions = $self->sqlSelectAllHashrefArray(

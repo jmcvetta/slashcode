005af2f67afcb45e6046711a8aa2270949679d2b
brian <brian>
2002-11-19 19:27:08 +0000

    Storylist stuff

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2002 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.255 2002/11/19 04:58:09 jamie Exp $
+# $Id: MySQL.pm,v 1.256 2002/11/19 19:27:08 brian Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -15,7 +15,7 @@ use vars qw($VERSION);
 use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 
-($VERSION) = ' $Revision: 1.255 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.256 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -5267,7 +5267,8 @@ sub getStoryList {
 		. 'time, name, stories.subsection,stories.section, displaystatus, stories.writestatus';
 	my $tables = "stories, topics";
 	my $where = "stories.tid=topics.tid ";
-	my $SECT = $self->getSection($user->{section} ? $user->{section} : $form->{section});
+	# See getSubmissionsForUser() on why the following is like this. -Brian
+	my $SECT = $self->getSection($user->{section} || $form->{section});
 	if ($SECT->{type} eq 'collected') {
 		$where .= " AND stories.section IN ('" . join("','", @{$SECT->{contained}}) . "')" 
 			if $SECT->{contained} && @{$SECT->{contained}};

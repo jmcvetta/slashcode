45b17de4fbc414aa1df99bca9063069874f0d508
jamie <jamie>
2002-11-22 06:56:32 +0000

    SQL fix.

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2002 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.264 2002/11/22 06:18:51 jamie Exp $
+# $Id: MySQL.pm,v 1.265 2002/11/22 06:56:32 jamie Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -15,7 +15,7 @@ use vars qw($VERSION);
 use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 
-($VERSION) = ' $Revision: 1.264 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.265 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -5401,7 +5401,8 @@ sub getRecentComments {
 	my $end_cid = $max_cid - $startat;
 	my $ar = $self->sqlSelectAllHashrefArray(
 		"comments.sid AS sid, comments.cid AS cid,
-		 date, ipid, subnetid, subject,
+		 date, comments.ipid AS ipid,
+		 comments.subnetid AS subnetid, subject,
 		 comments.uid AS uid, points AS score,
 		 lastmod, comments.reason AS reason,
 		 users.nickname AS nickname,

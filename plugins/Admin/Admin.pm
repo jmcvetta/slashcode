a3da6ed2da6fcf65c2aa987e6de0ebd6b0259a8e
jamie <jamie>
2002-12-13 01:43:25 +0000

    Get rid of thresh_secs, it's dumb.

--- plugins/Admin/Admin.pm
+++ plugins/Admin/Admin.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2002 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: Admin.pm,v 1.4 2002/12/13 01:01:25 jamie Exp $
+# $Id: Admin.pm,v 1.5 2002/12/13 01:43:25 jamie Exp $
 
 package Slash::Admin;
 
@@ -15,7 +15,7 @@ use base 'Exporter';
 use base 'Slash::DB::Utility';
 use base 'Slash::DB::MySQL';
 
-($VERSION) = ' $Revision: 1.4 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.5 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # On a side note, I am not sure if I liked the way I named the methods either.
 # -Brian
@@ -43,7 +43,6 @@ sub getAccesslogAbusersByID {
 	my($self, $options) = @_;
 	my $min_id = $options->{min_id} || 0;
 	my $thresh_count = $options->{thresh_count} || 100;
-	my $thresh_secs = $options->{thresh_secs} || 5;
 	my $thresh_hps = $options->{thresh_hps} || 0.1;
 	my $limit = 500;
 	my $ar = $self->sqlSelectAllHashrefArray(
@@ -55,7 +54,6 @@ sub getAccesslogAbusersByID {
 		"id >= $min_id",
 		"GROUP BY host_addr,op
 		 HAVING c >= $thresh_count
-			AND secs >= $thresh_secs
 			AND hps >= $thresh_hps
 		 ORDER BY maxts DESC, c DESC
 		 LIMIT $limit"

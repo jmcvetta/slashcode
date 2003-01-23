c0fe67c6cddefd85651341ab3bd9ba9448ad248c
pater <pater>
2003-01-23 16:20:58 +0000

    removed breaking code so IPID pages work again while I retool the IPID page.

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2002 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.304 2003/01/21 21:34:38 pudge Exp $
+# $Id: MySQL.pm,v 1.305 2003/01/23 16:20:58 pater Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -16,7 +16,7 @@ use vars qw($VERSION);
 use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 
-($VERSION) = ' $Revision: 1.304 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.305 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -3176,7 +3176,6 @@ sub getNumCommPostedByUID {
 sub getUIDList {
 	my($self, $column, $id) = @_;
 
-	my @result = ( );
 	my $where = '';
 	$id = md5_hex($id) if length($id) != 32;
 	if ($column eq 'md5id') {
@@ -3188,12 +3187,7 @@ sub getUIDList {
 		return [ ];
 	}
 
-	push @result, $self->sqlSelectAll("DISTINCT uid ", "submissions", $where);
-	push @result, $self->sqlSelectAll("DISTINCT uid ", "comments", $where);
-	push @result, $self->sqlSelectAll("DISTINCT uid ", "moderatorlog", $where);
-
-	# unique uids only, please
-	return [ keys %{{ map { $_ => 1 } @result }} ];
+	return $self->sqlSelectAll("DISTINCT uid ", "comments", $where);
 }
 
 ##################################################################

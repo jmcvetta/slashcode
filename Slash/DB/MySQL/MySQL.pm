59ab31751284ae89806dd797cb05c6dc02e8ce4f
jamie <jamie>
2003-01-31 07:03:16 +0000

    Skip accesslog caching for admins.

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2002 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.315 2003/01/31 05:46:04 jamie Exp $
+# $Id: MySQL.pm,v 1.316 2003/01/31 07:03:16 jamie Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -16,7 +16,7 @@ use vars qw($VERSION);
 use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 
-($VERSION) = ' $Revision: 1.315 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.316 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -1378,7 +1378,7 @@ sub createAccessLog {
 		secure		=> Slash::Apache::ConnectionIsSecure(),
 		referer		=> $ENV{HTTP_REFERER},
 	};
-	if ($constants->{accesslog_insert_cachesize}) {
+	if ($constants->{accesslog_insert_cachesize} && !$user->{is_admin}) {
 		# Save up multiple accesslog inserts until we can do them all at once.
 		push @{$self->{_accesslog_insert_cache}}, $insert;
 		my $size = scalar(@{$self->{_accesslog_insert_cache}});

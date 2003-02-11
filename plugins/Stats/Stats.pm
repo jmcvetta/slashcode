fe0b5da37a93d091afb506a8f072e25ce57becd2
jamie <jamie>
2003-02-11 20:19:05 +0000

    New subscriber-related stats and information.

--- plugins/Stats/Stats.pm
+++ plugins/Stats/Stats.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2002 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: Stats.pm,v 1.96 2003/02/11 20:05:32 brian Exp $
+# $Id: Stats.pm,v 1.97 2003/02/11 20:19:05 jamie Exp $
 
 package Slash::Stats;
 
@@ -22,7 +22,7 @@ use vars qw($VERSION);
 use base 'Slash::DB::Utility';
 use base 'Slash::DB::MySQL';
 
-($VERSION) = ' $Revision: 1.96 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.97 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # On a side note, I am not sure if I liked the way I named the methods either.
 # -Brian
@@ -789,6 +789,23 @@ sub countDailySubscribers {
 }
 
 ########################################################
+sub getStatToday {
+	my($self, $name) = @_;
+	my $name_q = $self->sqlQuote($name);
+	return $self->sqlSelect("value + 0", "stats_daily",
+		"name = $name_q AND day = '$self->{_day}'");
+}
+
+########################################################
+sub getStatLastNDays {
+	my($self, $name, $days) = @_;
+	my $name_q = $self->sqlQuote($name);
+	return $self->sqlSelect("AVG(value + 0)", "stats_daily",
+		"name = $name_q
+		 AND day BETWEEN DATE_SUB('$self->{_day}', INTERVAL $days DAY) AND '$self->{_day}'");
+}
+
+########################################################
 sub getDurationByStaticOpHour {
 	my($self, $options) = @_;
 
@@ -1089,4 +1106,4 @@ Slash(3).
 
 =head1 VERSION
 
-$Id: Stats.pm,v 1.96 2003/02/11 20:05:32 brian Exp $
+$Id: Stats.pm,v 1.97 2003/02/11 20:19:05 jamie Exp $

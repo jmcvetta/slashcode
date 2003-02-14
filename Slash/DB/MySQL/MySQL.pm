28e2fd5ffc3c43e4a003f18f8ca77f033674a0e0
pudge <pudge>
2003-02-14 04:29:41 +0000

    Remove debugging code

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2002 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.329 2003/02/11 19:15:34 pudge Exp $
+# $Id: MySQL.pm,v 1.330 2003/02/14 04:29:41 pudge Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -16,7 +16,7 @@ use vars qw($VERSION);
 use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 
-($VERSION) = ' $Revision: 1.329 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.330 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -7078,9 +7078,6 @@ sub DESTROY {
 
 	# flush accesslog insert cache
 	if (ref $self->{_accesslog_insert_cache}) {
-
-		printf STDERR "%s exiting; inserting %d rows into accesslog\n",
-			$$, scalar @{$self->{_accesslog_insert_cache}};
 
 		$self->sqlDo("SET AUTOCOMMIT=0");
 		while (my $hr = shift @{$self->{_accesslog_insert_cache}}) {

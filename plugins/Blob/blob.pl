1fb8b0e488384815a71d66b04370f64f9240657f
brian <brian>
2003-02-21 20:23:20 +0000

    Saving name change

--- plugins/Blob/blob.pl
+++ plugins/Blob/blob.pl
@@ -2,11 +2,12 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2002 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: blob.pl,v 1.1 2003/02/21 19:49:35 brian Exp $
+# $Id: blob.pl,v 1.2 2003/02/21 20:23:20 brian Exp $
 
 use strict;
 use Slash;
 use Slash::Search;
+use Slash::Blob;
 use Slash::Display;
 use Slash::Utility;
 use Slash::XML;
@@ -19,17 +20,19 @@ sub main {
 	my $blob = getObject("Slash::Blob", { db_type => 'reader' });
 	
 	unless ($form->{id}) {
-		redirect("404.pl");
+		redirect("/404.pl");
+		return;
 	}
 	
 	my $data = $blob->get($form->{id});
 	if (!$data || $user->{seclev} < $data->{seclev}) {
-		redirect("404.pl");
+		redirect("/404.pl");
+		return;
 	}
 
 	my $r = Apache->request;
 	$r->header_out('Cache-Control', 'private');
-	$r->content_type($data->{mime_type});
+	$r->content_type($data->{content_type});
 	$r->status(200);
 	$r->send_http_header;
 	$r->rflush;

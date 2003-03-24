c29bdd4fbf4917f9e4289a9ce6fb365bb7946ef0
pudge <pudge>
2003-03-24 19:46:39 +0000

    New quote

--- Slash/Apache/User/User.pm
+++ Slash/Apache/User/User.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2003 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: User.pm,v 1.63 2003/03/17 20:09:54 pudge Exp $
+# $Id: User.pm,v 1.64 2003/03/24 19:46:39 pudge Exp $
 
 package Slash::Apache::User;
 
@@ -22,7 +22,7 @@ use vars qw($REVISION $VERSION @ISA @QUOTES $USER_MATCH $request_start_time);
 
 @ISA		= qw(DynaLoader);
 $VERSION   	= '2.003000';  # v2.3.0
-($REVISION)	= ' $Revision: 1.63 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($REVISION)	= ' $Revision: 1.64 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 bootstrap Slash::Apache::User $VERSION;
 
@@ -619,6 +619,7 @@ Bender:Crippling pain? That's not covered by my insurance fraud.
 Bender:Let's commence preparations for rumbling!
 Bender:Woohoo, I'm popular!
 Bender:Ah crap, I'm some sort of robot!
+Bender:When will man learn that all races are equally inferior to robots?
 Fry:There's a lot about my face you don't know.
 Fry:Drugs are for losers. And hypnosis is for losers with big weird eyebrows.
 Fry:These new hands are great. I'm gonna break them in tonight.

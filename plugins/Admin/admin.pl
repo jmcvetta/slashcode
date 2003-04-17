f125a5da85f4c6f6f1037e11c3557322b5ee304c
pudge <pudge>
2003-04-17 16:47:12 +0000

    Fix template edit after template search (slash bug #723141)

--- plugins/Admin/admin.pl
+++ plugins/Admin/admin.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2003 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: admin.pl,v 1.147 2003/04/16 20:38:19 brian Exp $
+# $Id: admin.pl,v 1.148 2003/04/17 16:47:12 pudge Exp $
 
 use strict;
 use File::Temp 'tempfile';
@@ -290,13 +290,13 @@ sub templateEdit {
 	my($title, $templateref, $template_select, $page_select,
 		$section_select, $savepage_select, $savesection_select);
 
-	my($templatedelete_flag, $templateedit_flag, $templateform_flag) = 0;
+	my($templatedelete_flag, $templateedit_flag, $templateform_flag) = (0, 0, 0);
 	my $pagehashref = {};
 	$title = getTitle('templateEdit-title', {}, 1);
 	#Just to punish those section only admins! -Brian
 	$form->{section} = $user->{section} if $user->{section};
 
-	if ($form->{templatenew} || $form->{templatepage} || $form->{templatesection} || $form->{templatepageandsection}) {
+	if ($form->{templatenew} || $form->{templatepage} || $form->{templatesection} || $form->{templatepageandsection} || $form->{templatesearch}) {
 		$tpid    = '';
 		$page    = $form->{page};
 		$section = $form->{section};
@@ -333,6 +333,7 @@ sub templateEdit {
 	$section ||= 'default';
 
 	$templateref = $slashdb->getTemplate($tpid, '', 1) if $tpid;
+
 	$seclev_flag = 0 if $templateref->{seclev} > $user->{seclev};
 
 	if ($form->{templatedelete}) {
@@ -356,8 +357,7 @@ sub templateEdit {
 		}
 
 		if ($form->{templatesearch}) {
-			$getsection = $getpage = $tpid = '';
-			$templateref = {};
+			$getsection = $getpage = '';
 			$form->{templatelastselect} = 'templatesearch';
 			$templates = $slashdb->getTemplateListByText($form->{'templatesearchtext'});
 		} else {

a86602d3ddaec05f6a6fa71c39966d5685bcf74e
pudge <pudge>
2003-05-20 19:39:59 +0000

    Don't get comment text if no comments

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2003 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.398 2003/05/20 18:45:37 jamie Exp $
+# $Id: MySQL.pm,v 1.399 2003/05/20 19:39:59 pudge Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -16,7 +16,7 @@ use vars qw($VERSION);
 use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 
-($VERSION) = ' $Revision: 1.398 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.399 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -4637,8 +4637,10 @@ sub getCommentText {
 		}
 
 		return \%return;
-	} else {
+	} elsif ($cid) {
 		return $self->sqlSelect("comment", "comment_text", "cid=$cid");
+	} else {
+		return {};
 	}
 }
 

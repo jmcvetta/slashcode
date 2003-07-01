237997a48260c7cad93e0dea396df6ab998884e4
pudge <pudge>
2003-07-01 21:55:41 +0000

    whitespace!  AIEEEEEE!

--- Slash/Utility/Data/Data.pm
+++ Slash/Utility/Data/Data.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2003 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: Data.pm,v 1.90 2003/07/01 20:42:05 jamie Exp $
+# $Id: Data.pm,v 1.91 2003/07/01 21:55:41 pudge Exp $
 
 package Slash::Utility::Data;
 
@@ -41,7 +41,7 @@ use XML::Parser;
 use base 'Exporter';
 use vars qw($VERSION @EXPORT);
 
-($VERSION) = ' $Revision: 1.90 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.91 $ ' =~ /\$Revision:\s+([^\s]+)/;
 @EXPORT	   = qw(
 	addDomainTags
 	createStoryTopicData
@@ -1512,7 +1512,7 @@ sub fudgeurl {
 	# modify scheme:/ to scheme:// for $schemes defined below
 	# need to recreate $uri after doing so to make userinfo
 	# clearing work for something like http:/foo.com...@bar.com
-	my $schemes_to_mod = { http=>1, https=>1, ftp=>1 };
+	my $schemes_to_mod = { http => 1, https => 1, ftp => 1 };
 	if ($scheme && $schemes_to_mod->{$scheme}) {
 		$url = $uri->canonical->as_string;
 		$url =~ s|^$scheme:/([^/])|$scheme://$1|;
@@ -2905,4 +2905,4 @@ Slash(3), Slash::Utility(3).
 
 =head1 VERSION
 
-$Id: Data.pm,v 1.90 2003/07/01 20:42:05 jamie Exp $
+$Id: Data.pm,v 1.91 2003/07/01 21:55:41 pudge Exp $

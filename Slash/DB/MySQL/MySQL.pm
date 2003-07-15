50e0a5b6a633537eb423790421d746deabaeba39
pudge <pudge>
2003-07-15 22:23:40 +0000

    the white!

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2003 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.422 2003/07/15 21:22:14 jamie Exp $
+# $Id: MySQL.pm,v 1.423 2003/07/15 22:23:40 pudge Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -16,7 +16,7 @@ use vars qw($VERSION);
 use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 
-($VERSION) = ' $Revision: 1.422 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.423 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -2390,8 +2390,8 @@ sub isPollOpen {
 
 #####################################################
 sub hasPollActivated{
-	my ($self, $qid) = @_;
-	return $self->sqlCount("pollquestions","qid='$qid' and date <= now()");
+	my($self, $qid) = @_;
+	return $self->sqlCount("pollquestions", "qid='$qid' and date <= now()");
 }
 
 

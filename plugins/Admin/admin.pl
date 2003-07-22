04ba42f4d6fc06096c6071d9e32b3ad80d6b5e06
pudge <pudge>
2003-07-22 22:25:17 +0000

    fix up the slashtags stuff in editStory

--- plugins/Admin/admin.pl
+++ plugins/Admin/admin.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2003 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: admin.pl,v 1.166 2003/07/10 16:43:02 pudge Exp $
+# $Id: admin.pl,v 1.167 2003/07/22 22:25:17 pudge Exp $
 
 use strict;
 use File::Temp 'tempfile';
@@ -1256,7 +1256,7 @@ sub editStory {
 		$storyref->{section}	    = $SECT->{section};
 		$storyref->{subsection}	    = $SECT->{defaultsubsection};
 
-		$storyref->{'time'} = $slashdb->getTime();
+		$storyref->{'time'} = $slashdb->getTime;
 		$storyref->{uid} = $user->{uid};
 		$storyref->{writestatus} = "dirty";
 		$subid = $form->{subid};
@@ -1274,7 +1274,15 @@ sub editStory {
 		for my $field (qw( introtext bodytext )) {
 			$storyref->{$field} = cleanSlashTags(
 				$storyref->{$field}, {});
-			$storyref->{$field} = processSlashTags(
+
+			# do some of the processing displayStory()
+			# does, as we are bypassing it by going straight to
+			# dispStory() -- pudge
+			$story_copy{$field} = parseSlashizedLinks(
+				$storyref->{$field}, {});
+			$story_copy{$field} = cleanSlashTags(
+				$storyref->{$field}, {});
+			$story_copy{$field} = processSlashTags(
 				$storyref->{$field}, {});
 		}
 

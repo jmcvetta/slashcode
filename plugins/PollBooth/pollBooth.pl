d9e143bb401f9d54010727a0c758f6a7d43f850b
pudge <pudge>
2003-07-29 18:45:49 +0000

    whitespace fixes

--- plugins/PollBooth/pollBooth.pl
+++ plugins/PollBooth/pollBooth.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2003 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: pollBooth.pl,v 1.53 2003/07/29 14:39:40 vroom Exp $
+# $Id: pollBooth.pl,v 1.54 2003/07/29 18:45:49 pudge Exp $
 
 use strict;
 use Slash;
@@ -110,18 +110,20 @@ sub editpoll {
 		return;
 	}
 
-	my($question, $answers, $pollbooth, $checked,$story_ref);
+	my($question, $answers, $pollbooth, $checked, $story_ref);
 	if ($form->{op} eq "preview") {
 		foreach (qw/question voters date section topic polltype/) {
 			$question->{$_} = $form->{$_};
 		}
-		$story_ref = $reader->sqlSelectHashref("sid,qid,time,section,tid,displaystatus","stories","sid=".$reader->sqlQuote
-($form->{sid})) if $form->{sid};
-                if($story_ref){
-                        $question->{'date'}=$story_ref->{'time'};
-                        $question->{topic}=$story_ref->{'tid'};
-                        $question->{section}=$story_ref->{section};
-                        $question->{polltype}=$story_ref->{displaystatus} >=0 ? "story" : "nodisplay";
+		$story_ref = $reader->sqlSelectHashref("sid,qid,time,section,tid,displaystatus",
+			"stories",
+			"sid=" . $reader->sqlQuote($form->{sid})
+		) if $form->{sid};
+		if ($story_ref) {
+			$question->{'date'}	= $story_ref->{'time'};
+			$question->{topic}	= $story_ref->{'tid'};
+			$question->{section}	= $story_ref->{section};
+			$question->{polltype}	= $story_ref->{displaystatus} >= 0 ? "story" : "nodisplay";
 		}
                 
 		$question->{polltype} ||= "section";
@@ -161,14 +163,16 @@ sub editpoll {
 		$question->{sid} = $slashdb->getSidForQID($qid)
 			unless $question->{autopoll} eq "yes";
 
-		if($question->{sid}){
-			$story_ref = $reader->sqlSelectHashref("sid,qid,time,section,tid,displaystatus","stories","sid=".$reader->sqlQuote
-($question->{sid}));
-                	if($story_ref){
-                        	$question->{'date'}=$story_ref->{'time'};
-                        	$question->{topic}=$story_ref->{'tid'};
-                        	$question->{section}=$story_ref->{section};
-                        	$question->{polltype}=$story_ref->{displaystatus} >=0 ? "story" : "nodisplay";
+		if ($question->{sid}) {
+			$story_ref = $reader->sqlSelectHashref("sid,qid,time,section,tid,displaystatus",
+				"stories",
+				"sid=" . $reader->sqlQuote($question->{sid})
+			);
+			if ($story_ref) {
+				$question->{'date'}	= $story_ref->{'time'};
+				$question->{topic}	= $story_ref->{'tid'};
+				$question->{section}	= $story_ref->{section};
+				$question->{polltype}	= $story_ref->{displaystatus} >= 0 ? "story" : "nodisplay";
 			}
                 }
 		$question->{polltype} ||= "section";
@@ -215,7 +219,7 @@ sub editpoll {
 	my $date = $question->{date};
         $date ||= $form->{date};
         $date ||= $slashdb->getTime();
-        my $topics=$reader->getDescriptions('topics_section');
+        my $topics = $reader->getDescriptions('topics_section');
 	slashDisplay('editpoll', {
 		title		=> getData('edit_poll_title', { qid=>$qid }),
 		qid		=> $qid,
@@ -223,7 +227,7 @@ sub editpoll {
 		answers		=> $answers,
 		pollbooth	=> $pollbooth,
 		checked		=> $checked,
-                date            => $date,
+                date		=> $date,
 		story		=> $story_ref,
 		topics		=> $topics
 	});

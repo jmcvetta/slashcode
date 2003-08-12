bdf29ff9939900d1392ea5610389472943224fa3
vroom <vroom>
2003-08-12 15:13:29 +0000

    Fixed friends/foes clearing bug

--- plugins/Zoo/Zoo.pm
+++ plugins/Zoo/Zoo.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2003 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: Zoo.pm,v 1.42 2003/05/06 22:28:11 brian Exp $
+# $Id: Zoo.pm,v 1.43 2003/08/12 15:13:29 vroom Exp $
 
 package Slash::Zoo;
 
@@ -16,7 +16,7 @@ use vars qw($VERSION @EXPORT);
 use base 'Slash::DB::Utility';
 use base 'Slash::DB::MySQL';
 
-($VERSION) = ' $Revision: 1.42 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.43 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # "There ain't no justice" -Niven
 # We can try. 	-Brian
@@ -282,7 +282,7 @@ sub rebuildUser {
 			}
 		}
 	}
-
+	$people ||= {};
 	return $people;
 }
 

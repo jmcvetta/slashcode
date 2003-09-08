9bc305bc0482e96a9b8742b036dc633bda0c4396
pater <pater>
2003-09-08 18:27:29 +0000

    changed regex to require / instead of letting it stay optional

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2003 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.446 2003/09/02 20:36:40 jamie Exp $
+# $Id: MySQL.pm,v 1.447 2003/09/08 18:27:29 pater Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -16,7 +16,7 @@ use vars qw($VERSION);
 use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 
-($VERSION) = ' $Revision: 1.446 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.447 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -1360,7 +1360,7 @@ sub createAccessLog {
 	$hostip = md5_hex($hostip);
 	my $subnetid = getCurrentUser('subnetid') || $hostip;
 
-	if ($op eq 'index' && $dat =~ /^([^\/]*)\/?.*?$/) {
+	if ($op eq 'index' && $dat =~ /^([^\/]*)\/.*?$/) {
 		$section = $1;
 	}
 

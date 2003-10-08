6f619d81b5d867df439bdf649ddc037d84318859
pater <pater>
2003-10-08 15:01:19 +0000

    updated fortune location for new cluster

--- themes/slashcode/tasks/new_motd.pl
+++ themes/slashcode/tasks/new_motd.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2003 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: new_motd.pl,v 1.4 2003/07/22 20:26:32 jamie Exp $
+# $Id: new_motd.pl,v 1.5 2003/10/08 15:01:19 pater Exp $
 
 use strict;
 
@@ -16,8 +16,8 @@ $task{$me}{code} = sub {
 
 	my($virtual_user, $constants, $slashdb, $user) = @_;
 
-	return unless -x '/usr/games/fortune';
-	chomp(my $t = `/usr/games/fortune -s`);
+	return unless -x '/usr/local/games/fortune';
+	chomp(my $t = `/usr/local/games/fortune -s`);
 
 	if ($t) {
 		my $tpid = $slashdb->getTemplateByName("motd", "tpid");

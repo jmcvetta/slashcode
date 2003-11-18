edd26545d8b9bb175fd650297b708a19f5ca20d4
pater <pater>
2003-11-18 22:04:14 +0000

    fixed typo

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2003 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.477 2003/11/18 20:41:04 pater Exp $
+# $Id: MySQL.pm,v 1.478 2003/11/18 22:04:14 pater Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -18,7 +18,7 @@ use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 use Slash::Constants ':messages';
 
-($VERSION) = ' $Revision: 1.477 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.478 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -8516,7 +8516,7 @@ sub getMenus {
 
 ########################################################
 # for ubb_like_forums
-sub getForumDescriptions {
+sub getForumDescription {
 	my($self, $forum_id);
 
 	my $desc = $self->sqlSelect('comment', 'comments, comment_text', "comments.cid=comment_text.cid AND sid=$forum_id", 'ORDER BY comments.cid ASC LIMIT 1');

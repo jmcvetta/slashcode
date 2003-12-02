7def5234b7989d0905b39509d1803af7341fbea1
vroom <vroom>
2003-12-02 17:53:32 +0000

    Added handling for "from" which allows a user to enter who the gift is being given by.  This value is then used in the e-mail sent to the receiving user.

--- plugins/Subscribe/subscribe.pl
+++ plugins/Subscribe/subscribe.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2003 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: subscribe.pl,v 1.28 2003/10/16 01:38:10 jamie Exp $
+# $Id: subscribe.pl,v 1.29 2003/12/02 17:53:32 vroom Exp $
 
 use strict;
 
@@ -220,7 +220,7 @@ sub makepayment {
 			"-hits_paidfor" => "hits_paidfor + $num_pages"
 		});
 		print "<p>makepayment: Payment confirmed\n";
-		send_gift_msg($payment->{uid}, $payment->{puid}, $payment->{pages})
+		send_gift_msg($payment->{uid}, $payment->{puid}, $payment->{pages}, $form->{from})
 			if $payment->{payment_type} eq "gift";
 	} else {
 		use Data::Dumper;
@@ -278,12 +278,13 @@ sub confirm {
 	slashDisplay("confirm", {
 		type     => $type,
 		uid      => $uid,
-		sub_user => $sub_user
+		sub_user => $sub_user,
+		from 	 => $form->{from}
 	});
 }
 
 sub send_gift_msg {
-	my ($uid, $puid, $pages) = @_;
+	my ($uid, $puid, $pages, $from) = @_;
 	
 	my $slashdb = getCurrentDB();
 	my $constants = getCurrentStatic();
@@ -294,7 +295,8 @@ sub send_gift_msg {
 	my $message = slashDisplay('gift_msg', {
 			receiving_user  => $receiving_user,
 			purchasing_user => $purchasing_user,
-			pages 		=> $pages	
+			pages 		=> $pages,
+			from		=> $from	
 		}, { Return => 1, Nocomm => 1 } );
 	my $title = "Gift subscription to $constants->{sitename}\n";
 	doEmail($uid, $title, $message);

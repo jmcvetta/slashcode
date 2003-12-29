7ba6d889d2ee64d5485a7e878712972a354289b5
jamie <jamie>
2003-12-29 22:31:48 +0000

    More randomness, and alphanumericity, for createLogToken.

--- Slash/Utility/Data/Data.pm
+++ Slash/Utility/Data/Data.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2003 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: Data.pm,v 1.103 2003/12/19 18:07:43 pudge Exp $
+# $Id: Data.pm,v 1.104 2003/12/29 22:31:48 jamie Exp $
 
 package Slash::Utility::Data;
 
@@ -41,7 +41,7 @@ use XML::Parser;
 use base 'Exporter';
 use vars qw($VERSION @EXPORT);
 
-($VERSION) = ' $Revision: 1.103 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.104 $ ' =~ /\$Revision:\s+([^\s]+)/;
 @EXPORT	   = qw(
 	addDomainTags
 	createStoryTopicData
@@ -400,22 +400,45 @@ sub timeCalc {
 
 =head2 createLogToken()
 
-Return new random 20-character logtoken, composed of hex chars.
+Return new random 20-character logtoken, composed of \w chars.
 
 =over 4
 
 =item Return value
 
-Random password.
+Return a random password that matches /^\w{20}$/.
+
+We're only pulling out 3 chars each time thru this loop, so we only
+need (and trust) about 18 bits worth of randomness.  We re-seed srand
+periodically to try to get more randomness into the mix ("it uses a
+semirandom value supplied by the kernel (if it supports the /dev/urandom
+device)", says the Camel book).  I don't think I'm doing anything
+mathematically dumb to introduce any predictability into this, so it
+should be fine, wasteful of a few microseconds perhaps, ugly perhaps, but
+the 20-char value it returns should have very close to 119 bits of
+randomness.
 
 =back
 
 =cut
 
 {
-	my $int = 2**32-1;
+	my $maxint = 2**32-1;
 	sub createLogToken {
-		return md5_base64(int rand $int);
+		my $str = "";
+		my $need_srand = 0;
+		while (length($str) < 20) {
+			if ($need_srand) {
+				srand();
+				$need_srand = 0;
+			}
+			my $r = rand($maxint) . ":" . rand($maxint);
+			my $md5 = md5_base64($r);
+			$md5 =~ s/\W//g;
+			$str .= substr($md5, int(rand 8) + 5, 3);
+			$need_srand = 1 if rand() < 0.3;
+		}
+		return substr($str, 0, 20);
 	}
 }
 
@@ -3097,4 +3120,4 @@ Slash(3), Slash::Utility(3).
 
 =head1 VERSION
 
-$Id: Data.pm,v 1.103 2003/12/19 18:07:43 pudge Exp $
+$Id: Data.pm,v 1.104 2003/12/29 22:31:48 jamie Exp $

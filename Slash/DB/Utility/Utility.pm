f0216faced2e21327bd0825200afd2b36644419f
jamiemccarthy <jamiemccarthy>
2004-02-02 07:29:56 +0000

    Don't throw an SQL error if no WHERE clause is passed to sqlUpdate.

--- Slash/DB/Utility/Utility.pm
+++ Slash/DB/Utility/Utility.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2003 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: Utility.pm,v 1.46 2003/10/16 01:38:10 jamie Exp $
+# $Id: Utility.pm,v 1.47 2004/02/02 07:29:56 jamiemccarthy Exp $
 
 package Slash::DB::Utility;
 
@@ -12,7 +12,7 @@ use DBIx::Password;
 use Time::HiRes;
 use vars qw($VERSION);
 
-($VERSION) = ' $Revision: 1.46 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.47 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # FRY: Bender, if this is some kind of scam, I don't get it.  You already
 # have my power of attorney.
@@ -718,7 +718,7 @@ sub sqlUpdate {
 		}
 	}
 	chop $sql; # lose the terminal ","
-	$sql .= "\nWHERE $where\n";
+	$sql .= "\nWHERE $where\n" if $where;
 	my $qlid = $self->_querylog_start("UPDATE", $table);
 	my $rows = $self->sqlDo($sql);
 	$self->_querylog_finish($qlid);

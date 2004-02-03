13b164fec1c0793ce8eaed329ad197ef353b5ffc
jamiemccarthy <jamiemccarthy>
2004-02-03 18:46:58 +0000

    Use _dbh->{AutoCommit} instead of SET AUTOCOMMIT.

--- Slash/DB/Utility/Utility.pm
+++ Slash/DB/Utility/Utility.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2003 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: Utility.pm,v 1.47 2004/02/02 07:29:56 jamiemccarthy Exp $
+# $Id: Utility.pm,v 1.48 2004/02/03 18:46:58 jamiemccarthy Exp $
 
 package Slash::DB::Utility;
 
@@ -12,7 +12,7 @@ use DBIx::Password;
 use Time::HiRes;
 use vars qw($VERSION);
 
-($VERSION) = ' $Revision: 1.47 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.48 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # FRY: Bender, if this is some kind of scam, I don't get it.  You already
 # have my power of attorney.
@@ -400,12 +400,12 @@ sub _querylog_writecache {
 		&& @{$self->{_querylog}{cache}};
 	my $dbh = $self->{_querylog}{db}{_dbh};
 	return unless $dbh;
-	$dbh->do("SET AUTOCOMMIT=0");
+	$dbh->{AutoCommit} = 0;
 	while (my $sql = shift @{$self->{_querylog}{cache}}) {
 		$dbh->do($sql);
 	}
-	$dbh->do("COMMIT");
-	$dbh->do("SET AUTOCOMMIT=1");
+	$dbh->commit;
+	$dbh->{AutoCommit} = 1;
 }
 
 ########################################################

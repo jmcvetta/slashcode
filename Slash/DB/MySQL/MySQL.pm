077e792e09c2b765fd768289407e50ee0cd22888
tvroom <tvroom>
2004-04-15 01:58:22 +0000

    Fix for dbAvailable check

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2004 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.559 2004/04/13 17:06:39 pudge Exp $
+# $Id: MySQL.pm,v 1.560 2004/04/15 01:58:22 tvroom Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -19,7 +19,7 @@ use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 use Slash::Constants ':messages';
 
-($VERSION) = ' $Revision: 1.559 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.560 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -4949,6 +4949,7 @@ sub countStory {
 
 sub moderateComment {
 	my($self, $sid, $cid, $reason, $options) = @_;
+	return 0 unless dbAvailable("write_comments");
 	return 0 unless $reason;
 	$options ||= {};
 

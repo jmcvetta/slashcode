700a7372ed6b954d44be6aa91dc15ee9467dd610
tvroom <tvroom>
2004-05-11 19:01:50 +0000

    Additions for ipid column in open_proxies table
    Var to control Lingua::Stem usage for uncommon story words

--- sql/mysql/defaults.sql
+++ sql/mysql/defaults.sql
@@ -3,7 +3,7 @@
 #--------------------------------------------------------
 # Server version	3.23.26-beta-log
 #
-# $Id: defaults.sql,v 1.172 2004/05/11 16:20:31 jamiemccarthy Exp $
+# $Id: defaults.sql,v 1.173 2004/05/11 19:01:50 tvroom Exp $
 #
 
 #
@@ -914,6 +914,7 @@ INSERT INTO vars (name, value, description) VALUES ('slogan','Slash Site','Sloga
 INSERT INTO vars (name, value, description) VALUES ('smtp_server','localhost','The mailserver for the site');
 INSERT INTO vars (name, value, description) VALUES ('stats_reports','admin@example.com','Who to send daily stats reports to');
 INSERT INTO vars (name, value, description) VALUES ('stats_sfnet_groupids','4421','List of sf.net group IDs to keep stats on');
+INSERT INTO vars (name, value, description) VALUES ('stem_uncommon_words', '1', 'Use stems of words for detecting similar stories instead of whole words?');
 INSERT INTO vars (name, value, description) VALUES ('stir','3','Number of days before unused moderator points expire');
 INSERT INTO vars (name, value, description) VALUES ('story_expire','600','Default expiration time for story cache');
 INSERT INTO vars (name, value, description) VALUES ('submiss_ts','1','print timestamp in submissions view');
--- sql/mysql/slashschema_create.sql
+++ sql/mysql/slashschema_create.sql
@@ -4,7 +4,7 @@
 #--------------------------------------------------------
 # Server version	3.23.26-beta
 #
-# $Id: slashschema_create.sql,v 1.148 2004/04/13 18:19:34 tvroom Exp $
+# $Id: slashschema_create.sql,v 1.149 2004/05/11 19:01:55 tvroom Exp $
 #
 
 #
@@ -526,8 +526,10 @@ CREATE TABLE open_proxies (
 	port	SMALLINT UNSIGNED NOT NULL DEFAULT '0',
 	ts	DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
 	xff	VARCHAR(15) DEFAULT NULL,
+	ipid char(32) DEFAULT '' NOT NULL,
 	PRIMARY KEY (ip),
-	KEY ts (ts)
+	KEY ts (ts),
+	KEY ipid (ipid)
 ) TYPE=InnoDB;
 
 #

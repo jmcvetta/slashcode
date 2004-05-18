01c170e13d59f6232e917e5235b400f5d62f093b
tvroom <tvroom>
2004-05-18 15:49:44 +0000

    Fix for refund payments

--- plugins/Subscribe/Subscribe.pm
+++ plugins/Subscribe/Subscribe.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2004 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: Subscribe.pm,v 1.30 2004/04/14 18:07:42 jamiemccarthy Exp $
+# $Id: Subscribe.pm,v 1.31 2004/05/18 15:49:44 tvroom Exp $
 
 package Slash::Subscribe;
 
@@ -15,7 +15,7 @@ use vars qw($VERSION);
 use base 'Slash::DB::Utility';
 use base 'Slash::DB::MySQL';
 
-($VERSION) = ' $Revision: 1.30 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.31 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 sub new {
 	my($class) = @_;
@@ -228,12 +228,13 @@ sub plummyPage {
 # By default, allow readers to buy x pages for $y, 2x pages for $2y,
 # etc.  If you want to have n-for-the-price-of-m sales or whatever,
 # change the logic here.
-# Also, if someone hacks the HTML to purchase less than one
+# Also, if someone hacks the HTML to purchase a fraction of a 
 # subscription, they get nothing.
 sub convertDollarsToPages {
 	my($self, $amount) = @_;
 	my $constants = getCurrentStatic();
-	$amount = 0 if $amount < $constants->{paypal_amount};
+	my $paypal_amt = $constants->{paypal_amount};
+	$amount = 0 if !$paypal_amt || ($amount / $paypal_amt) != int($amount / $paypal_amt);
 	return sprintf("%0.0f", $amount*$constants->{paypal_num_pages}/
 		$constants->{paypal_amount});
 }

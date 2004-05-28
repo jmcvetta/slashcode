010af82af2e9f489410456dc2b7fc2d37cc5178e
jamiemccarthy <jamiemccarthy>
2004-05-28 16:22:03 +0000

    If no cid available, take "cid >=" clause out of select instead of making it "cid >= 0" (call me paranoid but I don't want to give the database any more reason to do something stupid :)

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2004 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.581 2004/05/28 15:22:47 tvroom Exp $
+# $Id: MySQL.pm,v 1.582 2004/05/28 16:22:03 jamiemccarthy Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -19,7 +19,7 @@ use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 use Slash::Constants ':messages';
 
-($VERSION) = ' $Revision: 1.581 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.582 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -4034,7 +4034,7 @@ sub getNumCommPostedAnonByIPID {
 	my($self, $ipid, $hours, $start_cid) = @_;
 	$ipid = $self->sqlQuote($ipid);
 	$hours ||= 24;
-	$start_cid ||= 0;
+	my $cid_clause = $start_cid ? " AND cid >= $start_cid" : "";
 	my $ac_uid = $self->sqlQuote(getCurrentStatic("anonymous_coward_uid"));
 	my $ar = $self->sqlSelectArrayRef(
 		"COUNT(*) AS count, SUM(pointsorig-points) AS sum",
@@ -4042,7 +4042,7 @@ sub getNumCommPostedAnonByIPID {
 		"ipid=$ipid
 		 AND uid=$ac_uid
 		 AND date >= DATE_SUB(NOW(), INTERVAL $hours HOUR)
-		 AND cid >= $start_cid"
+		 $cid_clause"
 	);
 	my($num_comm, $sum_mods) = @$ar;
 	$sum_mods ||= 0;
@@ -4061,13 +4061,13 @@ sub getNumCommPostedByUID {
 	my($self, $uid, $hours, $start_cid) = @_;
 	$uid = $self->sqlQuote($uid);
 	$hours ||= 24;
-	$start_cid ||= 0;
+	my $cid_clause = $start_cid ? " AND cid >= $start_cid" : "";
 	my $ar = $self->sqlSelectArrayRef(
 		"COUNT(*) AS count, SUM(points-pointsorig) AS sum",
 		"comments",
 		"uid=$uid
 		 AND date >= DATE_SUB(NOW(), INTERVAL $hours HOUR)
-		 AND cid >= $start_cid"
+		 $cid_clause"
 	);
 	my($num_comm, $sum_mods) = @$ar
 		if ref($ar) eq 'ARRAY';
@@ -6558,16 +6558,16 @@ sub getNetIDKarma {
 	my($self, $type, $id) = @_;
 	my($count, $karma);
 	if ($type eq "ipid") {
-		($count, $karma) = $self->sqlSelect("count(*),sum(karma)","comments","ipid='$id'");
+		($count, $karma) = $self->sqlSelect("COUNT(*),sum(karma)","comments","ipid='$id'");
 		return wantarray ? ($karma, $count) : $karma;
 	} elsif ($type eq "subnetid") {
-		($count, $karma) = $self->sqlSelect("count(*),sum(karma)","comments","subnetid='$id'");
+		($count, $karma) = $self->sqlSelect("COUNT(*),sum(karma)","comments","subnetid='$id'");
 		return wantarray ? ($karma, $count) : $karma;
 	} else {
-		($count, $karma) = $self->sqlSelect("count(*),sum(karma)","comments","ipid='$id'");
+		($count, $karma) = $self->sqlSelect("COUNT(*),sum(karma)","comments","ipid='$id'");
 		return wantarray ? ($karma, $count) : $karma if $count;
 
-		($count, $karma) = $self->sqlSelect("count(*),sum(karma)","comments","subnetid='$id'");
+		($count, $karma) = $self->sqlSelect("COUNT(*),sum(karma)","comments","subnetid='$id'");
 		return wantarray ? ($karma, $count) : $karma;
 	}
 }

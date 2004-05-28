659522c8e72a29a6ec4c35d988db91c9c1db6add
tvroom <tvroom>
2004-05-28 15:14:57 +0000

    Updated set_cids to keep track of cid for comments that are one day old.

--- themes/slashcode/tasks/set_cids.pl
+++ themes/slashcode/tasks/set_cids.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2004 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: set_cids.pl,v 1.6 2004/04/02 00:43:06 pudge Exp $
+# $Id: set_cids.pl,v 1.7 2004/05/28 15:14:57 tvroom Exp $
 
 use strict;
 use vars qw( %task $me );
@@ -13,7 +13,7 @@ use Slash::Display;
 use Slash::Utility;
 use Slash::Constants ':slashd';
 
-(my $VERSION) = ' $Revision: 1.6 $ ' =~ /\$Revision:\s+([^\s]+)/;
+(my $VERSION) = ' $Revision: 1.7 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 $task{$me}{timespec} = '5-59/10 * * * *';
 $task{$me}{timespec_panic_1} = ''; # not that important
@@ -22,27 +22,32 @@ $task{$me}{fork} = SLASHD_NOWAIT;
 # Handles saving useful cids so we can speed up certain selects later.
 $task{$me}{code} = sub {
 	my($virtual_user, $constants, $slashdb, $user) = @_;
-	my $days = $slashdb->getVar("admin_comment_display_days", "value", 1) || 30;
+	
+	my @days = (1);
+	push @days, ($slashdb->getVar("admin_comment_display_days", "value", 1) || 30);
 
-	# Get the old value for this var, or if it hasn't been
-	# defined, create it at the default 0.
-	my $start_at = $slashdb->getVar("min_cid_last_$days\_days", "value", 1);
-	if (!defined($start_at)){
-		$slashdb->createVar("min_cid_last_$days\_days", 0);
-		$start_at ||= 0;
-	}
+	foreach my $days (@days) {
 
-	# Now get the new value for this var;  if the method
-	# doesn't return a new value, then there were no new
-	# comments posted during that period several days ago,
-	# so we just keep using the old value.
-	my $cid = $slashdb->getCidForDaysBack($days, $start_at) || $start_at;
-	my $success = $slashdb->setVar("min_cid_last_$days\_days", $cid);
+		# Get the old value for this var, or if it hasn't been
+		# defined, create it at the default 0.
+		my $start_at = $slashdb->getVar("min_cid_last_$days\_days", "value", 1);
+		if (!defined($start_at)){
+			$slashdb->createVar("min_cid_last_$days\_days", 0);
+			$start_at ||= 0;
+		}
 
-	if ($success) {
-		return "min_cid_last_$days\_days is $cid";
-	} else {
-		return "could not set min_cid_last_$days\_days to $cid";
+		# Now get the new value for this var;  if the method
+		# doesn't return a new value, then there were no new
+		# comments posted during that period several days ago,
+		# so we just keep using the old value.
+		my $cid = $slashdb->getCidForDaysBack($days, $start_at) || $start_at;
+		my $success = $slashdb->setVar("min_cid_last_$days\_days", $cid);
+
+		if ($success) {
+			slashdLog("min_cid_last_$days\_days is $cid");
+		} else {
+			slashdLog("could not set min_cid_last_$days\_days to $cid");
+		}
 	}
 };
 

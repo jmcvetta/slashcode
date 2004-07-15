e90903cc13398e0a11d7939830ee5ffd137e832d
jamiemccarthy <jamiemccarthy>
2004-07-15 04:24:05 +0000

    Remove Never Display and Revert Weights buttons, and replace with the [ ] Display checkbox.  Also shuffle some other admin story UI elements around.

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2004 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.620 2004/07/13 21:56:23 jamiemccarthy Exp $
+# $Id: MySQL.pm,v 1.621 2004/07/15 04:24:05 jamiemccarthy Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -19,7 +19,7 @@ use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 use Slash::Constants ':messages';
 
-($VERSION) = ' $Revision: 1.620 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.621 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -3391,7 +3391,10 @@ sub setStory {
 		# If a topics_chosen hashref was given, we write not just that,
 		# but also topics_rendered, primaryskid and tid.
 		$self->setStoryTopicsChosen($stoid, $chosen_hr);
-		my($primaryskid, $tids) = $self->setStoryRenderedFromChosen($stoid, $chosen_hr);
+		my $info_hr = { };
+		$info_hr->{neverdisplay} = 1 if $hashref->{neverdisplay};
+		my($primaryskid, $tids) = $self->setStoryRenderedFromChosen($stoid, $chosen_hr,
+			$info_hr);
 		$hashref->{primaryskid} = $primaryskid;
 		$hashref->{tid} = $tids->[0] || 0;
 	}
@@ -6922,7 +6925,10 @@ sub createStory {
 		$error = "Failed to set chosen topics for story '$stoid'\n" if !$success;
 	}
 	if (!$error) {
-		($primaryskid, $tids) = $self->setStoryRenderedFromChosen($stoid, $story->{topics_chosen});
+		my $info_hr = { };
+		$info_hr->{neverdisplay} = 1 if $story->{neverdisplay};
+		($primaryskid, $tids) = $self->setStoryRenderedFromChosen($stoid,
+			$story->{topics_chosen}, $info_hr);
 		$error = "Failed to set rendered topics for story '$stoid'\n" if !defined($primaryskid);
 	}
 	delete $story->{topics_chosen};
@@ -8383,7 +8389,7 @@ sub getStoryTopicsRendered {
 # whose remaining elements are the topics in topiclist order.
 # Pass in $chosen_hr to save a query.
 sub setStoryRenderedFromChosen {
-	my($self, $stoid, $chosen_hr) = @_;
+	my($self, $stoid, $chosen_hr, $info) = @_;
 
 	$chosen_hr ||= $self->getStoryTopicsChosen($stoid);
 	my $rendered_hr = $self->renderTopics($chosen_hr);
@@ -8392,12 +8398,14 @@ sub setStoryRenderedFromChosen {
 		{ topics_chosen => $chosen_hr });
 
 	$self->sqlDelete("story_topics_rendered", "stoid = $stoid");
-	for my $key (sort keys %$rendered_hr) {
-		unless ($self->sqlInsert("story_topics_rendered", 
-			{ stoid => $stoid, tid => $key, weight => $rendered_hr->{$key} }
-		)) {
-			# and we should ROLLBACK here
-			return undef;
+	if (!$info->{neverdisplay}) {
+		for my $key (sort keys %$rendered_hr) {
+			unless ($self->sqlInsert("story_topics_rendered", 
+				{ stoid => $stoid, tid => $key, weight => $rendered_hr->{$key} }
+			)) {
+				# and we should ROLLBACK here
+				return undef;
+			}
 		}
 	}
 

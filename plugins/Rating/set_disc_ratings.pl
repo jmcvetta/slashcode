59c9cc055d32bfc9b9b927a020712f3379b3c9ee
tvroom <tvroom>
2004-09-07 20:02:17 +0000

    Updated to work with schema changes

--- plugins/Rating/set_disc_ratings.pl
+++ plugins/Rating/set_disc_ratings.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2004 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: set_disc_ratings.pl,v 1.3 2004/09/06 19:30:02 jamiemccarthy Exp $
+# $Id: set_disc_ratings.pl,v 1.4 2004/09/07 20:02:17 tvroom Exp $
 
 use strict;
 
@@ -11,14 +11,14 @@ use Slash::Constants ':slashd';
 use vars qw( %task $me );
 
 
-$task{$me}{timespec} = '0-59/10 * * * 6';
+$task{$me}{timespec} = '0-59/10 * * * *';
 $task{$me}{timespec_panic_1} = ''; # if panic, this can wait
 $task{$me}{fork} = SLASHD_NOWAIT;
 $task{$me}{code} = sub {
 	my($virtualuser, $constants, $slashdb, $user, $info, $gSkin) = @_;
 
-	my $ratings_reader = getObject('Slash::Ratings', { db_type => 'reader' });
-	my $ratings_writer = getObject('Slash::Ratings');
+	my $ratings_reader = getObject('Slash::Rating', { db_type => 'reader' });
+	my $ratings_writer = getObject('Slash::Rating');
 
 	my $last_max = $slashdb->getVar('set_disc_rating_last_cid', 'value', 1) || 0;
 	my $this_min = $last_max + 1; # don't count the last-counted vote twice
@@ -26,6 +26,7 @@ $task{$me}{code} = sub {
 	return "no new comments" if $this_max < $this_min;
 
 	my $discussions  = $ratings_reader->getUniqueDiscussionsBetweenCids($this_min, $this_max);
+	
 	my $num_replaces = $ratings_writer->updateDiscussionRatingStats($discussions);
 
 	$slashdb->setVar("set_disc_rating_last_cid", $this_max);

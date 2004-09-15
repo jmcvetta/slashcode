81342092817437950a9ee000094e7e821d5867d8
jamiemccarthy <jamiemccarthy>
2004-09-15 15:02:29 +0000

    s_t_r.weight problem fixed

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2004 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.679 2004/09/14 23:34:14 jamiemccarthy Exp $
+# $Id: MySQL.pm,v 1.680 2004/09/15 15:02:29 jamiemccarthy Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -19,7 +19,7 @@ use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 use Slash::Constants ':messages';
 
-($VERSION) = ' $Revision: 1.679 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.680 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -9628,7 +9628,7 @@ sub setStoryRenderedFromChosen {
 	if (!$info->{neverdisplay}) {
 		for my $key (sort keys %$rendered_hr) {
 			unless ($self->sqlInsert("story_topics_rendered", 
-				{ stoid => $stoid, tid => $key, weight => $rendered_hr->{$key} }
+				{ stoid => $stoid, tid => $key }
 			)) {
 				# and we should ROLLBACK here
 				return undef;

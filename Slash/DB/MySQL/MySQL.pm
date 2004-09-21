5459a89bedfd501d770bfbb81a0e3affe87a7f25
tvroom <tvroom>
2004-09-21 14:47:53 +0000

    Committed option to filter out downmodded comments.

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2004 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.684 2004/09/21 01:51:49 jamiemccarthy Exp $
+# $Id: MySQL.pm,v 1.685 2004/09/21 14:47:53 tvroom Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -19,7 +19,7 @@ use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 use Slash::Constants ':messages';
 
-($VERSION) = ' $Revision: 1.684 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.685 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -6708,6 +6708,8 @@ sub getCommentsForUser {
 		$where .= "  OR cid=$cid"			if $cid;
 		$where .= ")";
 	}
+
+	$where .= " AND points >= pointsorig " if $options->{skip_downmodded};
 
 	my $comments = $self->sqlSelectAllHashrefArray($select, $tables, $where, $other);
 

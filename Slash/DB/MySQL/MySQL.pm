23561ac210c5d644924d867d5fdc4f306b91808f
pudge <pudge>
2004-11-09 21:39:34 +0000

    oops

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2004 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.726 2004/11/09 20:20:12 pudge Exp $
+# $Id: MySQL.pm,v 1.727 2004/11/09 21:39:34 pudge Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -19,7 +19,7 @@ use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 use Slash::Constants ':messages';
 
-($VERSION) = ' $Revision: 1.726 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.727 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -2717,7 +2717,7 @@ sub getDB {
 	my($self, $db_type) = @_;
 
 	my $dbs = $self->getDBs;
-	my @found = [ grep { $_->{isalive} eq 'yes'} @{$dbs->{$db_type}} ];
+	my @found = grep { $_->{isalive} eq 'yes' } @{$dbs->{$db_type}};
 
 	return $found[rand @found]{virtual_user};
 }

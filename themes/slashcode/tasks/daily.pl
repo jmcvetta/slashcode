1bb46e5b504ba65c659e7b64f3cfd946383d5297
jamiemccarthy <jamiemccarthy>
2004-11-22 23:10:50 +0000

    Bugfix that prevented (I think) some users from getting daily mails

--- themes/slashcode/tasks/daily.pl
+++ themes/slashcode/tasks/daily.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2004 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: daily.pl,v 1.12 2004/10/08 01:24:28 jamiemccarthy Exp $
+# $Id: daily.pl,v 1.13 2004/11/22 23:10:50 jamiemccarthy Exp $
 
 use strict;
 
@@ -59,7 +59,7 @@ sub daily_generateDailyMailees {
 				story_never_topic	story_never_author	story_never_nexus
 				story_always_topic	story_always_author	story_always_nexus
 			)}) {
-				$key .= '|' . join(',', sort m/'(.+?)'/g);
+				$key .= '|' . join(',', sort( $value =~ /'(.+?)'/g ));
 			}
 			# allow us to make certain emails sent individually,
 			# by including a unique value in users_param for
@@ -140,15 +140,18 @@ sub daily_mailingList {
 
 	my $mailings	= daily_generateDailyMailees($n_users, $h_users) or return;
 
-	for my $mailing (keys %$mailings) {
+	for my $mailing (sort keys %$mailings) {
 		my $subj  = $mailings->{$mailing}{subj};
 		my $code  = $mailings->{$mailing}{code};
 		my $mkeys = $mailings->{$mailing}{mkeys};
 
-		slashdLog("Daily Mail ($mailing) begin");
-		for my $key (keys %$mkeys) {
-			my $user  = $mkeys->{$key}{user};
+		slashdLog("Daily Mail ($mailing) begin for " . scalar(keys %$mkeys) . " mkeys");
+		for my $key (sort keys %$mkeys) {
+			my $user = $mkeys->{$key}{user};
 			my $text = daily_generateDailyMail($mailing, $user, $constants, $slashdb) or next;
+			if (my $n_users = scalar(@{ $mkeys->{$key}{mails} }) >= 100) {
+				slashdLog("Sending " . length($text) . " bytes to $n_users users: '$key'");
+			}
 			$messages->bulksend(
 				$mkeys->{$key}{mails}, $subj,
 				$text, $code, $user->{uid}

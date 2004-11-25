c7a2a935af6f8846b6e52c6d0990ed0352f59a04
jamiemccarthy <jamiemccarthy>
2004-11-25 15:06:00 +0000

    Bugfix

--- themes/slashcode/htdocs/comments.pl
+++ themes/slashcode/htdocs/comments.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2004 by Open Source Development Network. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: comments.pl,v 1.211 2004/11/24 21:05:50 jamiemccarthy Exp $
+# $Id: comments.pl,v 1.212 2004/11/25 15:06:00 jamiemccarthy Exp $
 
 use strict;
 use Slash 2.003;	# require Slash 2.3.x
@@ -941,11 +941,17 @@ sub submitComment {
 
 #print STDERR scalar(localtime) . " $$ E header_emitted=$header_emitted do_emit_html=$do_emit_html redirect_to=" . (defined($redirect_to) ? $redirect_to : "undef") . "\n";
 
-	my $pts = $user->{defaultpoints};
+	# Set starting points to the AC's starting points, by default.
+	# If the user is posting under their own name, we'll reset this
+	# value (and add other modifiers) in a moment.
+	my $pts = getCurrentAnonymousCoward('defaultpoints');
+
 	my $karma_bonus = 0;
 	my $subscriber_bonus = 0;
 	my $tweak = 0;
 	if (!$user->{is_anon} && !$form->{postanon}) {
+
+		$pts = $user->{defaultpoints};
 
 		if ($constants->{karma_posting_penalty_style} == 0) {
 			$pts-- if $user->{karma} < 0;

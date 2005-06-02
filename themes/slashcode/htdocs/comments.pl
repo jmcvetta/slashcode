e218ffcf9a867e798f2d388d829b1b37448a6914
jamiemccarthy <jamiemccarthy>
2005-06-02 00:58:43 +0000

    New three-value range for hc_sw_comments, 0-2 instead of 0-1.

--- themes/slashcode/htdocs/comments.pl
+++ themes/slashcode/htdocs/comments.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: comments.pl,v 1.226 2005/06/01 21:03:16 tvroom Exp $
+# $Id: comments.pl,v 1.227 2005/06/02 00:58:43 jamiemccarthy Exp $
 
 use strict;
 use Slash 2.003;	# require Slash 2.3.x
@@ -219,12 +219,24 @@ sub main {
 		# yeah, the next step is to loop through the array of $ops->{$op}{check}
 		my $formname;
 		my $options = {};
+
+		# Disable HumanConf, if...
 		$options->{no_hc} = 1 if
+				# HumanConf is not running...
 			   !$constants->{plugin}{HumanConf}
 			|| !$constants->{hc}
-			|| !$constants->{hc_sw_comments}
-			|| (!$user->{is_anon}
-			   && $user->{karma} > $constants->{hc_maxkarma});
+				# ...or it's turned off for comments...
+			|| $constants->{hc_sw_comments} == 0
+				# ...or it's turned off for logged-in users
+				# and this user is logged-in...
+			|| $constants->{hc_sw_comments} == 1
+			   && !$user->{is_anon}
+				# ...or it's turned off for logged-in users
+				# with high enough karma, and this user
+				# qualifies.
+			|| $constants->{hc_sw_comments} == 2
+			   && !$user->{is_anon}
+			   &&  $user->{karma} > $constants->{hc_maxkarma};
  
 		my $done = 0;
 		DO_CHECKS: while (!$done) {

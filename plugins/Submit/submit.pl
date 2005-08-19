530c4002a84aca8a3cdb5408d325b4174d15aa1e
tvroom <tvroom>
2005-08-19 15:26:03 +0000

    Misc submit updates

--- plugins/Submit/submit.pl
+++ plugins/Submit/submit.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: submit.pl,v 1.120 2005/08/12 21:37:48 pudge Exp $
+# $Id: submit.pl,v 1.121 2005/08/19 15:26:03 tvroom Exp $
 
 use strict;
 use Slash 2.003;	# require Slash 2.3.x
@@ -195,8 +195,10 @@ sub deleteSubmissions {
 #################################################################
 sub blankForm {
 	my($constants, $slashdb, $user, $form) = @_;
+	print getData("submit_body_open");
 	yourPendingSubmissions(@_);
 	displayForm($user->{nickname}, $user->{fakeemail}, $form->{skin}, getData('defaulthead'));
+	print getData("submit_body_close");
 }
 
 #################################################################
@@ -210,13 +212,14 @@ sub yourPendingSubmissions {
 	my($constants, $slashdb, $user, $form) = @_;
 
 	return if $user->{is_anon};
-
+	print getData("submit_body_open");
 	if (my $submissions = $slashdb->getSubmissionsByUID($user->{uid}, "", { limit_days => 365 })) {
 		slashDisplay('yourPendingSubs', {
 			submissions	=> $submissions,
 			width		=> '100%',
 		});
 	}
+	print getData("submit_body_close");
 }
 
 #################################################################

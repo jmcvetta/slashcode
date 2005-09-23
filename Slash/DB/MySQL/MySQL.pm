3937a5bda40ea856b36405a9811fd967e9f69da3
jamiemccarthy <jamiemccarthy>
2005-09-23 17:21:48 +0000

    Comment to clarify a subtle interaction with $user->{lastclick}

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.799 2005/09/22 23:55:18 jamiemccarthy Exp $
+# $Id: MySQL.pm,v 1.800 2005/09/23 17:21:48 jamiemccarthy Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -19,7 +19,7 @@ use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 use Slash::Constants ':messages';
 
-($VERSION) = ' $Revision: 1.799 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.800 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -11880,6 +11880,8 @@ sub _getUser_do_selects {
 	}
 	# And adjust the users_hits.lastclick value, a timestamp,
 	# to work the same in 4.1 and later as it did in 4.0.
+	# This is vital to make a Slash::Apache::Log::UserLog
+	# test work properly.
 	$answer->{lastclick} =~ s/\D+//g if $answer->{lastclick};
 
 	return $answer;

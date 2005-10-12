f074cacdd0b5de3c70326123070af5052b98187d
pudge <pudge>
2005-10-12 17:38:25 +0000

    How did this not get done the first time?

--- plugins/ResKey/ResKey/Checks/ProxyScan.pm
+++ plugins/ResKey/ResKey/Checks/ProxyScan.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: ProxyScan.pm,v 1.3 2005/09/20 21:53:32 pudge Exp $
+# $Id: ProxyScan.pm,v 1.4 2005/10/12 17:38:25 pudge Exp $
 
 package Slash::ResKey::Checks::ProxyScan;
 
@@ -13,16 +13,16 @@ use Slash::Constants ':reskey';
 
 use base 'Slash::ResKey::Key';
 
-our($VERSION) = ' $Revision: 1.3 $ ' =~ /\$Revision:\s+([^\s]+)/;
+our($VERSION) = ' $Revision: 1.4 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 sub doCheck {
 	my($self) = @_;
 
-	my $constants = getCurrentStatic();
 	my $slashdb = getCurrentDB();
 	my $user = getCurrentUser();
+	my $check_vars = $self->getCheckVars;
 
-	if ($constants->{"reskey_checks_adminbypass_$self->{resname}"} && $user->{is_admin}) {
+	if ($check_vars->{adminbypass} && $user->{is_admin}) {
 		return RESKEY_SUCCESS;
 	}
 

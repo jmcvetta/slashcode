8ed00554aaa3f84dbdf06926a145cbd6f6501419
pudge <pudge>
2005-11-03 03:56:49 +0000

    sqlConnect

--- Slash/DB/Utility/Utility.pm
+++ Slash/DB/Utility/Utility.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: Utility.pm,v 1.64 2005/08/28 17:30:15 jamiemccarthy Exp $
+# $Id: Utility.pm,v 1.65 2005/11/03 03:56:49 pudge Exp $
 
 package Slash::DB::Utility;
 
@@ -12,7 +12,7 @@ use DBIx::Password;
 use Time::HiRes;
 use vars qw($VERSION);
 
-($VERSION) = ' $Revision: 1.64 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.65 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # FRY: Bender, if this is some kind of scam, I don't get it.  You already
 # have my power of attorney.
@@ -748,6 +748,8 @@ sub sqlSelectAllKeyValue {
 	$sql .= "FROM $from " if $from;
 	$sql .= "WHERE $where " if $where;
 	$sql .= "$other" if $other;
+
+	$self->sqlConnect() or return undef;
 
 	my $qlid = $self->_querylog_start("SELECT", $from);
 	my $H = $self->{_dbh}->selectall_arrayref($sql);

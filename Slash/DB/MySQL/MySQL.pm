cb346d91ba5518d50ef3c431fb785ac2d13bb970
jamiemccarthy <jamiemccarthy>
2006-01-07 21:43:15 +0000

    Eliminate 'uninitialized' warning (doing a setUser() to set a param to undef deletes it just like setting a param to the empty string, but now it does it without throwing a warning)

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.841 2006/01/06 19:52:13 jamiemccarthy Exp $
+# $Id: MySQL.pm,v 1.842 2006/01/07 21:43:15 jamiemccarthy Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -19,7 +19,7 @@ use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 use Slash::Constants ':messages';
 
-($VERSION) = ' $Revision: 1.841 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.842 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -11526,7 +11526,7 @@ sub setUser {
 	# What is worse, a select+update or a replace?
 	# I should look into that. (REPLACE is faster) -Brian
 	for (@param)  {
-		if ($_->[1] eq "") {
+		if (!defined($_->[1]) || $_->[1] eq "") {
 			$rows += $self->sqlDelete('users_param', 
 				"uid = $uid AND name = " . $self->sqlQuote($_->[0]));
 		} elsif ($_->[0] eq "acl") {

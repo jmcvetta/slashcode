5fda0996c1a5e34c27c585309f44ce59ed70990b
tvroom <tvroom>
2006-02-14 21:26:16 +0000

    Updates for signoff

--- plugins/Admin/admin.pl
+++ plugins/Admin/admin.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: admin.pl,v 1.278 2006/02/08 04:11:35 pudge Exp $
+# $Id: admin.pl,v 1.279 2006/02/14 21:26:16 tvroom Exp $
 
 use strict;
 use File::Temp 'tempfile';
@@ -189,6 +189,12 @@ sub main {
 			adminmenu	=> 'info',
 			tab_selected	=> 'mcdstats',
 		},
+		signoff_stats 		=> {
+			function	=> \&displaySignoffStats,
+			seclev		=> 500,
+			adminmenu	=> 'info',
+			tab_selected	=> 'signnoff'
+		}
 	};
 
 	# admin.pl is not for regular users
@@ -2390,6 +2396,32 @@ sub findTheTime {
 	# article.pl gets written first.
 	$time =~ s/( \d\d:\d\d):\d\d$/$1:00/;
 	return $time;
+}
+
+sub displaySignoffStats {
+	my ($form, $slashdb, $user, $constants) = @_;
+	my $admin = getObject('Slash::Admin');
+
+	my %stoids_for_days;
+	my $author_info;
+	my $num_days = [7, 30, 90 ];
+	for my $days (7,30,90) {
+		my $signoff_info = $admin->getSignoffData($days);
+		foreach (@$signoff_info) {
+			$author_info->{$_->{uid}}{nickname} = $_->{nickname};
+			$author_info->{$_->{uid}}{$days}{cnt}++;	
+			$author_info->{$_->{uid}}{$days}{tot_time} += $_->{min_to_sign};
+			$stoids_for_days{$days}{$_->{stoid}}++;
+			push @{$author_info->{$_->{uid}}{$days}{mins}}, $_->{min_to_sign};
+		}
+	}
+
+	slashDisplay("signoff_stats", {
+		author_info => $author_info,
+		stoids_for_days => \%stoids_for_days,
+		num_days  => $num_days
+	});
+
 }
 
 createEnvironment();

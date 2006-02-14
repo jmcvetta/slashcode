8fe0b3fadfbcf0599879ddbee197d5e458f3d2e4
tvroom <tvroom>
2006-02-14 19:47:17 +0000

    Updated order for tagbody show

--- plugins/Ajax/htdocs/images/common.js
+++ plugins/Ajax/htdocs/images/common.js
@@ -1,4 +1,4 @@
-// $Id: common.js,v 1.16 2006/02/14 19:14:27 tvroom Exp $
+// $Id: common.js,v 1.17 2006/02/14 19:47:17 tvroom Exp $
 
 function toggleIntro(id, toggleid) {
 	var obj = $(id);
@@ -65,12 +65,12 @@ function tagsShowBody(stoid, is_admin, newtagspreloadtext) {
 	// Make the body of the tagbox visible
 	var tagsbodyid = 'toggletags-body-' + stoid;
         var tagsbody = $(tagsbodyid);
-	tagsbody.className = "tagbody";
-	
 	toppos = tagbox.offsetTop + "px";
 	tagbox.style.position = "absolute";
 	tagbox.style.top = toppos;
-
+	
+	tagsbody.className = "tagbody";
+	
 	// If the tags-user div hasn't been filled, fill it.
 	var tagsuserid = 'tags-user-' + stoid;
 	var tagsuser = $(tagsuserid);

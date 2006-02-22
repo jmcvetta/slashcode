701453277e434552e7da69727027e4f068edada4
pudge <pudge>
2006-02-22 20:57:48 +0000

    revert admin.pl to not use regexSid()

--- plugins/Admin/admin.pl
+++ plugins/Admin/admin.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: admin.pl,v 1.283 2006/02/21 22:36:29 pudge Exp $
+# $Id: admin.pl,v 1.284 2006/02/22 20:57:48 pudge Exp $
 
 use strict;
 use File::Temp 'tempfile';
@@ -1480,14 +1480,19 @@ sub extractRelatedStoriesFromForm {
 		$related = [ $form->{related_story} ];
 	}
 
-	my $regexsid = regexSid();
+# XXX this is broken because regexSid() matches things other than sid
+# but in theory, should use regexSid() ...
+#	my $regexsid = regexSid();
+
+	my $match = qr/(?:$constants->{basedomain})?\S*(\d\d\/\d\d\/\d\d\/\d+)/;
+
 	if ($form->{add_related}) {
 		my @add_related = split('\n', $form->{add_related});
 		foreach (@add_related) {
 			s/^\s+|\s+$//g;
 			next if !$_;
 			# XXX should use regexSid()
-			if (/(?:$constants->{basedomain})?\S*$regexsid/) {
+			if ($_ =~ $match) {
 				push @$related, $1;
 			} else {
 				my($title, $url) = $_ =~ /^(.*)\s+(\S+)$/;
@@ -1499,7 +1504,7 @@ sub extractRelatedStoriesFromForm {
 
 	# Extract sids from urls in introtext and bodytext
 	foreach ($form->{introtext}, $form->{bodytext}) {
-		push @$related, $1 while /(?:$constants->{basedomain})?\S*$regexsid/g;
+		push @$related, $1 while /$match/g;
 	}
 
 	# should probably filter and check that they're actually sids, etc...

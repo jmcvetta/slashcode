64586ed6d52bee23151a9532985e013d18d34e93
tvroom <tvroom>
2006-04-18 20:30:24 +0000

    Updates for popular bookmarks page / bookmark fetch task

--- plugins/Bookmark/mysql_dump.sql
+++ plugins/Bookmark/mysql_dump.sql
@@ -1,9 +1,9 @@
 #
-# $Id: mysql_dump.sql,v 1.5 2006/04/05 20:20:43 pudge Exp $
+# $Id: mysql_dump.sql,v 1.6 2006/04/18 20:30:24 tvroom Exp $
 #
 
 INSERT IGNORE INTO reskey_vars VALUES (8,'duration_creation-use', 2, 'min duration (in seconds) between creation and use');
-INSERT IGNORE INTO reskey_vars VALUES (8,'user_seclev', 2, 'Minimum seclev to use resource');
+INSERT IGNORE INTO reskey_vars VALUES (8,'user_seclev', 1, 'Minimum seclev to use resource');
 INSERT IGNORE INTO reskey_vars VALUES (8, 'duration_uses', 30, 'min duration (in seconds) between uses');
 INSERT IGNORE INTO reskey_vars VALUES (8, 'duration_max-uses', '30', 'how many uses per timeframe');
 

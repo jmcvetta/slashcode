3c3c6b6c349e042af57f45ec98e2fccfe1d67a4b
tvroom <tvroom>
2006-07-05 17:59:09 +0000

    Update for new style of readmore links

--- themes/slashcode/htdocs/index.pl
+++ themes/slashcode/htdocs/index.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: index.pl,v 1.155 2006/04/10 19:29:26 pudge Exp $
+# $Id: index.pl,v 1.156 2006/07/05 17:59:09 tvroom Exp $
 
 use strict;
 use Slash;
@@ -699,9 +699,21 @@ sub displayStories {
 		$tmpreturn .= displayStory($story->{sid}, '', $other, $stories_data_cache);
 		
 		if ($other->{dispmode} eq "full") {
-
+			
+			my $readmore = $msg->{readmore};
+			if ($constants->{index_readmore_with_bytes}) {
+				my $readmore_data = {};
+				if ($story->{body_length}) {
+					if ($constants->{body_bytes}) {
+						$readmore_data->{bytes} = $story->{body_length};
+					} else {
+						$readmore_data->{words} = $story->{word_count};
+					}
+					$readmore = getData('readmore_with_bytes', $readmore_data );
+				}
+			}
 			push @links, linkStory({
-				'link'		=> $msg->{readmore},
+				'link'		=> $readmore,
 				sid		=> $story->{sid},
 				tid		=> $story->{tid},
 				skin		=> $story->{primaryskid},
@@ -716,13 +728,15 @@ sub displayStories {
 			}
 	
 			if ($story->{body_length} || $story->{commentcount}) {
-				push @links, linkStory({
-					'link'		=> $link,
-					sid		=> $story->{sid},
-					tid		=> $story->{tid},
-					mode		=> 'nocomment',
-					skin		=> $story->{primaryskid},
-				}, '', $ls_other) if $story->{body_length};
+				if (!$constants->{index_readmore_with_bytes}) {
+					push @links, linkStory({
+						'link'		=> $link,
+						sid		=> $story->{sid},
+						tid		=> $story->{tid},
+						mode		=> 'nocomment',
+						skin		=> $story->{primaryskid},
+					}, '', $ls_other) if $story->{body_length};
+				}
 
 				my @commentcount_link;
 				my $thresh = $threshComments[$user->{threshold} + 1];

9d0ff154e6778d0abacdc69030dff52b64fca757
cowboyneal <cowboyneal>
2006-08-04 14:56:40 +0000

    more sub memory niggles

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.906 2006/08/04 14:12:56 cowboyneal Exp $
+# $Id: MySQL.pm,v 1.907 2006/08/04 14:56:40 cowboyneal Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -19,7 +19,7 @@ use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 use Slash::Constants ':messages';
 
-($VERSION) = ' $Revision: 1.906 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.907 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -13683,14 +13683,14 @@ sub updateSubMemory {
 	my $rows = $self->sqlUpdate('submissions_notes', {
 		subnote	=> $subnote,
 		uid	=> $user->{uid},
-		'time'	=> 'NOW()',
+		'-time'	=> 'NOW()',
 	}, "submatch=" . $self->sqlQuote($submatch));
 
 	$self->sqlInsert('submissions_notes', {
 		submatch	=> $submatch,
 		subnote		=> $subnote,
 		uid		=> $user->{uid},
-		'time'		=> 'NOW()',
+		'-time'		=> 'NOW()',
 	}) if !$rows;
 }
 

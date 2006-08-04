6983d0de7f3aa8790ae70ae72dcacb924286b429
cowboyneal <cowboyneal>
2006-08-04 14:12:56 +0000

    one more sub memory fix

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.905 2006/08/04 14:06:59 cowboyneal Exp $
+# $Id: MySQL.pm,v 1.906 2006/08/04 14:12:56 cowboyneal Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -19,7 +19,7 @@ use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 use Slash::Constants ':messages';
 
-($VERSION) = ' $Revision: 1.905 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.906 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -13686,7 +13686,7 @@ sub updateSubMemory {
 		'time'	=> 'NOW()',
 	}, "submatch=" . $self->sqlQuote($submatch));
 
-	$self->sqlInsert('submissions_memory', {
+	$self->sqlInsert('submissions_notes', {
 		submatch	=> $submatch,
 		subnote		=> $subnote,
 		uid		=> $user->{uid},

93f75c44cff95b6f17b97d5a12527d969a2418f7
cowboyneal <cowboyneal>
2006-08-04 14:06:59 +0000

    fixed submissions memory insert

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: MySQL.pm,v 1.904 2006/08/01 16:23:37 jamiemccarthy Exp $
+# $Id: MySQL.pm,v 1.905 2006/08/04 14:06:59 cowboyneal Exp $
 
 package Slash::DB::MySQL;
 use strict;
@@ -19,7 +19,7 @@ use base 'Slash::DB';
 use base 'Slash::DB::Utility';
 use Slash::Constants ':messages';
 
-($VERSION) = ' $Revision: 1.904 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.905 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # Fry: How can I live my life if I can't tell good from evil?
 
@@ -13680,11 +13680,11 @@ sub updateSubMemory {
 
 	my $user = getCurrentUser();
 
-	my $rows = $self->sqlUpdate('submissions_memory', {
+	my $rows = $self->sqlUpdate('submissions_notes', {
 		subnote	=> $subnote,
 		uid	=> $user->{uid},
 		'time'	=> 'NOW()',
-	}, "submatch='" . $self->sqlQuote($submatch) . "'");
+	}, "submatch=" . $self->sqlQuote($submatch));
 
 	$self->sqlInsert('submissions_memory', {
 		submatch	=> $submatch,

4fe3591b99942e401b1f8cf230806135aec55dc0
tvroom <tvroom>
2006-09-06 17:53:22 +0000

    Fixed problem with saving Back

--- plugins/FireHose/FireHose.pm
+++ plugins/FireHose/FireHose.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: FireHose.pm,v 1.9 2006/09/05 20:46:38 tvroom Exp $
+# $Id: FireHose.pm,v 1.10 2006/09/06 17:53:22 tvroom Exp $
 
 package Slash::FireHose;
 
@@ -36,7 +36,7 @@ use base 'Slash::DB::Utility';
 use base 'Slash::DB::MySQL';
 use vars qw($VERSION);
 
-($VERSION) = ' $Revision: 1.9 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.10 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 sub createFireHose {
 	my($self, $data) = @_;
@@ -524,7 +524,7 @@ sub setSectionTopicsFromTagstring {
 
 	my %categories = map { ($_, $_) } (qw(hold quik),
 		(ref $constants->{submit_categories}
-			? @{$constants->{submit_categories}}
+			? map {lc($_)} @{$constants->{submit_categories}}
 			: ()
 		)
 	);
@@ -700,4 +700,4 @@ Slash(3).
 
 =head1 VERSION
 
-$Id: FireHose.pm,v 1.9 2006/09/05 20:46:38 tvroom Exp $
+$Id: FireHose.pm,v 1.10 2006/09/06 17:53:22 tvroom Exp $

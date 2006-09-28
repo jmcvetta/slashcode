12d5a2c9f91717e593578256bf9ec7dd4821f0e3
tvroom <tvroom>
2006-09-28 21:03:49 +0000

    Added code to encode type of tag / autocomplete menu needed

--- plugins/FireHose/FireHose.pm
+++ plugins/FireHose/FireHose.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: FireHose.pm,v 1.23 2006/09/28 19:12:52 tvroom Exp $
+# $Id: FireHose.pm,v 1.24 2006/09/28 21:03:49 tvroom Exp $
 
 package Slash::FireHose;
 
@@ -36,7 +36,7 @@ use base 'Slash::DB::Utility';
 use base 'Slash::DB::MySQL';
 use vars qw($VERSION);
 
-($VERSION) = ' $Revision: 1.23 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.24 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 sub createFireHose {
 	my($self, $data) = @_;
@@ -865,14 +865,18 @@ sub getFireHoseTagsTop {
 	my $constants 	= getCurrentStatic();
 	my $tags_top	 = [];
 	push @$tags_top, ($item->{type});
-	push @$tags_top, ($item->{category} || "none") if $user->{is_admin} && !$user->{firehose_usermode};
+	if ($user->{is_admin} && !$user->{firehose_usermode}) {
+		my $cat = $item->{category} || "none";
+		$cat .= ":1";
+		push @$tags_top, $cat;
+	}
 	if ($item->{primaryskid} && $item->{primaryskid} != $constants->{mainpage_skid}) {
 		my $the_skin = $self->getSkin($item->{primaryskid});
-		push @$tags_top, $the_skin->{name};
+		push @$tags_top, "$the_skin->{name}:2";
 	}
 	if ($item->{tid}) {
 		my $the_topic = $self->getTopic($item->{tid});
-		push @$tags_top, $the_topic->{keyword};
+		push @$tags_top, "$the_topic->{keyword}:3";
 	}
 	return $tags_top;
 }
@@ -890,4 +894,4 @@ Slash(3).
 
 =head1 VERSION
 
-$Id: FireHose.pm,v 1.23 2006/09/28 19:12:52 tvroom Exp $
+$Id: FireHose.pm,v 1.24 2006/09/28 21:03:49 tvroom Exp $

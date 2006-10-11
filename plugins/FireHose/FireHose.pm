c18372145c68abd1a75c202f00557b51705ad4d0
tvroom <tvroom>
2006-10-11 16:42:21 +0000

    Updates to set accepted bit for stories

--- plugins/FireHose/FireHose.pm
+++ plugins/FireHose/FireHose.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: FireHose.pm,v 1.30 2006/10/11 16:37:41 tvroom Exp $
+# $Id: FireHose.pm,v 1.31 2006/10/11 16:42:21 tvroom Exp $
 
 package Slash::FireHose;
 
@@ -36,7 +36,7 @@ use base 'Slash::DB::Utility';
 use base 'Slash::DB::MySQL';
 use vars qw($VERSION);
 
-($VERSION) = ' $Revision: 1.30 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.31 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 sub createFireHose {
 	my($self, $data) = @_;
@@ -154,7 +154,8 @@ sub createItemFromSubmission {
 			subnetid 		=> $submission->{subnetid},
 			email			=> $submission->{email},
 			emaildomain		=> $submission->{emaildomain},
-			name			=> $submission->{name}
+			name			=> $submission->{name},
+			accepted		=> "yes",
 		};
 		$self->createFireHose($data);
 	}
@@ -1041,4 +1042,4 @@ Slash(3).
 
 =head1 VERSION
 
-$Id: FireHose.pm,v 1.30 2006/10/11 16:37:41 tvroom Exp $
+$Id: FireHose.pm,v 1.31 2006/10/11 16:42:21 tvroom Exp $

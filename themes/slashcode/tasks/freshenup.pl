ab7d17a26fd7fb5e55dcef388bc0c0e7b3fbcfc6
cowboyneal <cowboyneal>
2006-12-05 15:14:22 +0000

    added missing )

--- themes/slashcode/tasks/freshenup.pl
+++ themes/slashcode/tasks/freshenup.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: freshenup.pl,v 1.67 2006/12/05 14:16:37 cowboyneal Exp $
+# $Id: freshenup.pl,v 1.68 2006/12/05 15:14:22 cowboyneal Exp $
 
 use File::Path;
 use File::Temp;
@@ -261,7 +261,7 @@ $task{$me}{code} = sub {
 		my($cchp_file, $cchp_param) = _make_cchp_file();
 
 		# update a story's audio version, if using cepstral audio.
-		if ($constants->{cepstral_audio} {
+		if ($constants->{cepstral_audio}) {
 			# fork a new script to render the audio, and
 			# it will update the story_param table with the correct
 			# pointers to the file

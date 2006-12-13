40c702d9a65e9ee4628aef9880d70cb91badcb38
jamiemccarthy <jamiemccarthy>
2006-12-13 18:50:50 +0000

    Bugfix

--- plugins/Login/login.pl
+++ plugins/Login/login.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: login.pl,v 1.24 2006/12/13 18:00:04 jamiemccarthy Exp $
+# $Id: login.pl,v 1.25 2006/12/13 18:50:50 jamiemccarthy Exp $
 
 use strict;
 use Slash 2.003;
@@ -12,7 +12,7 @@ use Slash::Utility;
 use Slash::XML;
 use vars qw($VERSION);
 
-($VERSION) = ' $Revision: 1.24 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.25 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 sub main {
 	my $slashdb   = getCurrentDB();
@@ -76,17 +76,13 @@ sub newUser {
 	my @note;
 	my $error = 0;
 
-	# check if new nick is OK
+	# check if new nick is OK and if user exists
+	my $matchname = nick2matchname($newnick);
 	my $newnick = nickFix($form->{newusernick});
 	if (!$newnick) {
 		push @note, getData('nick_invalid');
 		$error = 1;
-	}
-
-	# check if user exists
-	my $matchname = nick2matchname($newnick);
-
-	if (!$form->{email} || !emailValid($form->{email})) {
+	} elsif (!$form->{email} || !emailValid($form->{email})) {
 		push @note, getData('email_invalid');
 		$error = 1;
 	} elsif ($form->{email} ne $form->{email2}) {

978980641c1f5b7adf75558d9783f05082021b67
tvroom <tvroom>
2007-02-23 20:27:52 +0000

    Rename user tab to nickname

--- plugins/FireHose/FireHose.pm
+++ plugins/FireHose/FireHose.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: FireHose.pm,v 1.93 2007/02/23 06:18:00 pudge Exp $
+# $Id: FireHose.pm,v 1.94 2007/02/23 20:27:52 tvroom Exp $
 
 package Slash::FireHose;
 
@@ -37,7 +37,7 @@ use base 'Slash::DB::Utility';
 use base 'Slash::DB::MySQL';
 use vars qw($VERSION);
 
-($VERSION) = ' $Revision: 1.93 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.94 $ ' =~ /\$Revision:\s+([^\s]+)/;
 sub createFireHose {
 	my($self, $data) = @_;
 	$data->{dept} ||= "";
@@ -1278,6 +1278,14 @@ sub getAndSetOptions {
 		my $data = {};
 		foreach (@tab_fields) {
 			$data->{$_} = $tab->{$_};
+			
+			if ($tab->{$_} eq "User" && $_ eq "tabname") {
+				$data->{$_} = $user->{nickname};
+				$data->{$_} =~ s/[^A-Za-z0-9_-]//g;
+				if(length $data->{$_} > 16) {
+					$data->{$_} = substr($data->{$_}, 0, 16);
+				}
+			}
 			foreach my $field (qw(uid nickname)) {
 				$data->{$_} =~ s/{$field}/$user->{$field}/g;
 			}
@@ -1695,4 +1703,4 @@ Slash(3).
 
 =head1 VERSION
 
-$Id: FireHose.pm,v 1.93 2007/02/23 06:18:00 pudge Exp $
+$Id: FireHose.pm,v 1.94 2007/02/23 20:27:52 tvroom Exp $

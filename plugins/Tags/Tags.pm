f0ea5910eefddb9b8fedbacdbd7d469a8f827fe8
jamiemccarthy <jamiemccarthy>
2007-07-06 00:58:07 +0000

    if a blank item id comes in, log a detailed error (seeing a few of these)

--- plugins/Tags/Tags.pm
+++ plugins/Tags/Tags.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: Tags.pm,v 1.68 2007/06/27 00:16:36 jamiemccarthy Exp $
+# $Id: Tags.pm,v 1.69 2007/07/06 00:58:07 jamiemccarthy Exp $
 
 package Slash::Tags;
 
@@ -16,7 +16,7 @@ use vars qw($VERSION);
 use base 'Slash::DB::Utility';
 use base 'Slash::DB::MySQL';
 
-($VERSION) = ' $Revision: 1.68 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.69 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 # FRY: And where would a giant nerd be? THE LIBRARY!
 
@@ -1153,6 +1153,12 @@ sub ajaxTagHistory {
 		my $itemid = $form->{id};
 		my $firehose = getObject("Slash::FireHose");
 		my $item = $firehose->getFireHose($itemid);
+		if (!$item || !$item->{globjid}) {
+			use Data::Dumper;
+			my($i_d, $f_d) = (Dumper($item), Dumper($form));
+			$i_d =~ s/\s+/ /g; $f_d =~ s/\s+/ /g;
+			warn "ajaxTagHistory blank item or globjid: $i_d $f_d";
+		}
 		$globjid = $item->{globjid};
 		my $tags = getObject("Slash::Tags");
 		($table, $id) = $tags->getGlobjTarget($globjid);

bcc128e33b7b52e0e8b5542dbcc5bd9758519241
tvroom <tvroom>
2007-07-18 16:35:30 +0000

    Appropriate updates for admin layout

--- plugins/Ajax/htdocs/images/common.js
+++ plugins/Ajax/htdocs/images/common.js
@@ -1,5 +1,5 @@
 // _*_ Mode: JavaScript; tab-width: 8; indent-tabs-mode: true _*_
-// $Id: common.js,v 1.129 2007/07/17 23:46:35 pudge Exp $
+// $Id: common.js,v 1.130 2007/07/18 16:35:30 tvroom Exp $
 
 var fh_play = 0;
 var fh_is_timed_out = 0;
@@ -424,6 +424,7 @@ function toggle_firehose_body(id, is_admin) {
 	params['id'] = id;
 	var fhbody = $('fhbody-'+id);
 	var fh = $('firehose-'+id);
+	var usertype = fh_is_admin ? " adminmode" : " usermode";
 	if (fhbody.className == "empty") {
 		var handlers = {
 			onComplete: function() { 
@@ -437,19 +438,19 @@ function toggle_firehose_body(id, is_admin) {
 			ajax_update(params, 'fhbody-'+id);
 		}
 		fhbody.className = "body";
-		fh.className = "article";
+		fh.className = "article" + usertype;
 		if (is_admin)
 			tagsShowBody(id, is_admin, '', "firehose");
 	} else if (fhbody.className == "body") {
 		fhbody.className = "hide";
-		fh.className = "briefarticle";
-		if (is_admin)
-			tagsHideBody(id);
+		fh.className = "briefarticle" + usertype;
+		/*if (is_admin)
+			tagsHideBody(id);*/
 	} else if (fhbody.className == "hide") {
 		fhbody.className = "body";
-		fh.className = "article";
-		if (is_admin)
-			tagsShowBody(id, is_admin, '', "firehose");
+		fh.className = "article" + usertype;
+		/*if (is_admin)
+			tagsShowBody(id, is_admin, '', "firehose"); */
 	}
 }
 

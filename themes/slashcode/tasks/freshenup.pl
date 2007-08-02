baae1b455c3ea8ed2d22c13b9ac35f732a4134c5
tvroom <tvroom>
2007-08-02 17:57:52 +0000

    *** empty log message ***

--- themes/slashcode/tasks/freshenup.pl
+++ themes/slashcode/tasks/freshenup.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: freshenup.pl,v 1.75 2007/05/29 20:09:06 tvroom Exp $
+# $Id: freshenup.pl,v 1.76 2007/08/02 17:57:52 tvroom Exp $
 
 use File::Path;
 use File::Temp;
@@ -409,7 +409,7 @@ $task{$me}{code} = sub {
 
 		if ($constants->{plugin}{FireHose}) {
 			gen_firehose_static($virtual_user, "index_firehose.shtml", $gSkin->{name}, "", {  skipmenu => 1, skippop => 1, fhfilter=> "story", duration => "7", mode => 'mixed', color => "black"  }); 
-			gen_firehose_static($virtual_user, "firehose.shtml", $gSkin->{name}, "", { duration => "7", mode => 'fulltitle', color => "blue"  }); 
+			gen_firehose_static($virtual_user, "firehose.shtml", $gSkin->{name}, "", { duration => "7", mode => 'mixed', color => "blue", nodates => '1'  }); 
 		}
 		$slashdb->markSkinClean($mp_skid);
 		delete $dirty_skins{$mp_skid};
@@ -439,7 +439,7 @@ $task{$me}{code} = sub {
 			});
 			if ($constants->{plugin}{FireHose}) {
 				gen_firehose_static($virtual_user, "index_firehose.shtml", $skin->{name}, $skinname, { skipmenu => 1, skippop => 1, fhfilter=> "'story $skin->{name}'", duration => "7", mode => 'mixed', color => "black"  }); 
-				gen_firehose_static($virtual_user, "firehose.shtml", $skin->{name}, $skinname, { duration => "7", mode => 'fulltitle', color => "blue", fhfilter => "'$skin->{name}'" }); 
+				gen_firehose_static($virtual_user, "firehose.shtml", $skin->{name}, $skinname, { duration => "7", mode => 'mixed', color => "blue", nodates => '1', fhfilter => "'$skin->{name}'" }); 
 			}
 
 			$slashdb->markSkinClean($key);

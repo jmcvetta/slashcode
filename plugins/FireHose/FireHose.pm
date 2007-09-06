b4dc618736f441e7f8bc5d8775c82db05ccf7424
tvroom <tvroom>
2007-09-06 16:29:47 +0000

    Fixed bug in usage box

--- plugins/FireHose/FireHose.pm
+++ plugins/FireHose/FireHose.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: FireHose.pm,v 1.164 2007/09/04 20:53:21 tvroom Exp $
+# $Id: FireHose.pm,v 1.165 2007/09/06 16:29:47 tvroom Exp $
 
 package Slash::FireHose;
 
@@ -42,7 +42,7 @@ use base 'Slash::DB::Utility';
 use base 'Slash::DB::MySQL';
 use vars qw($VERSION);
 
-($VERSION) = ' $Revision: 1.164 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.165 $ ' =~ /\$Revision:\s+([^\s]+)/;
 sub createFireHose {
 	my($self, $data) = @_;
 	$data->{dept} ||= "";
@@ -2330,7 +2330,7 @@ sub ajaxFireHoseUsage {
 	my $downlabel = $constants->{tags_downvote_tagname} || 'nix';
 	my $down_id = $tags->getTagnameidFromNameIfExists($downlabel);
 	
-	my $uplabel = $constants->{tags_downvote_tagname} || 'nod';
+	my $uplabel = $constants->{tags_upvote_tagname} || 'nod';
 	my $up_id = $tags->getTagnameidFromNameIfExists($uplabel);
 	my $data = {};
 
@@ -2363,4 +2363,4 @@ Slash(3).
 
 =head1 VERSION
 
-$Id: FireHose.pm,v 1.164 2007/09/04 20:53:21 tvroom Exp $
+$Id: FireHose.pm,v 1.165 2007/09/06 16:29:47 tvroom Exp $

e7a0ee2e28cc0162404f9e42c36c9ea40cb2dbb8
pudge <pudge>
2007-09-14 21:52:28 +0000

    lengthen time

--- plugins/SearchToo/search_index.pl
+++ plugins/SearchToo/search_index.pl
@@ -2,7 +2,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: search_index.pl,v 1.8 2007/07/16 11:52:31 jamiemccarthy Exp $
+# $Id: search_index.pl,v 1.9 2007/09/14 21:52:28 pudge Exp $
 
 use strict;
 
@@ -23,7 +23,7 @@ $task{$me}{code} = sub {
 	$searchtoo->copyBackup;
 	$searchtoo->backup(1);
 	my $backup_duration = time - $backup_start_time;
-	if ($backup_duration > 30) {
+	if ($backup_duration > 60) {
 		slashdErrnote("backup process took $backup_duration seconds; file cleanup may be required");
 	}
 

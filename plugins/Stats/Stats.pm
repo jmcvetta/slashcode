668d848995d7b754267a84ceb60026ca278a83a8
jamiemccarthy <jamiemccarthy>
2008-01-15 20:26:23 +0000

    Bugfix

--- plugins/Stats/Stats.pm
+++ plugins/Stats/Stats.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: Stats.pm,v 1.187 2008/01/07 16:15:52 jamiemccarthy Exp $
+# $Id: Stats.pm,v 1.188 2008/01/15 20:26:23 jamiemccarthy Exp $
 
 package Slash::Stats;
 
@@ -22,7 +22,7 @@ use vars qw($VERSION);
 use base 'Slash::DB::Utility';
 use base 'Slash::DB::MySQL';
 
-($VERSION) = ' $Revision: 1.187 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.188 $ ' =~ /\$Revision:\s+([^\s]+)/;
 
 sub new {
 	my($class, $user, $options) = @_;
@@ -1710,7 +1710,7 @@ sub getTopBadgeHosts {
 	for my $duple (@$top_ar) {
 		my($uri, $c) = @$duple;
 		my $uri_obj = URI->new($uri);
-		my $host = $uri_obj ? $uri_obj->host() : $uri;
+		my $host = $uri_obj && $uri_obj->can('host') ? $uri_obj->host() : $uri;
 		$count{$host} += $c;
 	}
 	my @top_hosts = (sort { $count{$b} <=> $count{$a} || $a <=> $b } keys %count);
@@ -2148,4 +2148,4 @@ Slash(3).
 
 =head1 VERSION
 
-$Id: Stats.pm,v 1.187 2008/01/07 16:15:52 jamiemccarthy Exp $
+$Id: Stats.pm,v 1.188 2008/01/15 20:26:23 jamiemccarthy Exp $

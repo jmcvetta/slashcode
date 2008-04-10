f0a828134add631aff45efbc69596a46b6f722c4
scc <scc>
2008-04-10 19:28:54 +0000

    Abstract get_etag()
    Make Etag generation explicitly a separate function, in case
    we ever want to change it from md5_hex.

--- Slash/Utility/Anchor/Anchor.pm
+++ Slash/Utility/Anchor/Anchor.pm
@@ -1,7 +1,7 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id: Anchor.pm,v 1.89 2007/06/27 00:12:14 jamiemccarthy Exp $
+# $Id: Anchor.pm,v 1.90 2008/04/10 19:28:54 scc Exp $
 
 package Slash::Utility::Anchor;
 
@@ -36,7 +36,7 @@ use Slash::Utility::Environment;
 use base 'Exporter';
 use vars qw($VERSION @EXPORT);
 
-($VERSION) = ' $Revision: 1.89 $ ' =~ /\$Revision:\s+([^\s]+)/;
+($VERSION) = ' $Revision: 1.90 $ ' =~ /\$Revision:\s+([^\s]+)/;
 @EXPORT	   = qw(
 	http_send
 	header
@@ -268,7 +268,7 @@ sub http_send {
 
 	if ($opt->{etag} || $opt->{do_etag}) {
 		if ($opt->{do_etag} && $opt->{content}) {
-			$opt->{etag} = md5_hex($opt->{content});
+			$opt->{etag} = get_etag($opt->{content});
 		}
 		$r->header_out('ETag', $opt->{etag});
 
@@ -305,7 +305,10 @@ sub http_send {
 	return 1;
 }
 
-
+sub get_etag {
+	my($content) = @_;
+	return md5_hex($content);
+}
 
 #========================================================================
 
@@ -749,4 +752,4 @@ Slash(3), Slash::Utility(3).
 
 =head1 VERSION
 
-$Id: Anchor.pm,v 1.89 2007/06/27 00:12:14 jamiemccarthy Exp $
+$Id: Anchor.pm,v 1.90 2008/04/10 19:28:54 scc Exp $
